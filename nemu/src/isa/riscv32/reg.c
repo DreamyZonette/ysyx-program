8491594c3ab8faab3fe24cb0b5e7cfe80bcaba01
tracer-ysyx <tracer@ysyx.org>
2025-01-18 21:08:01 +0800

    > compile NEMU ysyx_100012104 龙文庆 Linux long-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:01 up 10:37,  8 users,  load average: 0.26, 0.32, 0.37

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ void isa_reg_display() {
 	int i;
 	int len = sizeof(regs) / sizeof(regs[0]);
 	for(i = 0 ; i < len ; i++){
-		printf(" %s",regs[i]);
+		printf(" %s",reg_name(i));
 	}
 	printf("\n");
 }
