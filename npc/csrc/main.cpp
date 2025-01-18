c9c263940d5f5bd1c3c4e233058bd38b62379adc
tracer-ysyx <tracer@ysyx.org>
2025-01-18 15:41:55 +0800

    > compile NEMU ysyx_100012104 龙文庆 Linux long-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:55 up  5:33,  6 users,  load average: 0.03, 0.05, 0.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,45 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include "verilated.h"
-#include "Vtop.h"
-// #include "include/nvboard.h"
 
-// static TOP_NAME dut;
-
-// void nvboard_bind_all_pins(TOP_NAME* top);
-
-/*static void single_cycle() { 
-	dut.clk = 0; dut.eval();
-	dut.clk = 1; dut.eval(); 
-}*/
-
-int main(int argc,char** argv)
-{
-	VerilatedContext* contextp = new VerilatedContext;
-	contextp->commandArgs(argc, argv);
-	Vtop* top = new Vtop{contextp};
-
-	//nvboard_bind_all_pins(&dut);
-	//nvboard_init();
-
-while (!contextp->gotFinish()) {
-  int a = rand() & 1;
-  int b = rand() & 1;
-  top->a = a;
-  top->b = b;
-  top->eval();
-  printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-  assert(top->f == (a ^ b));
-}
-
-delete top;
-delete contextp;
-
-/*while(1){
-	nvboard_update();
-	single_cycle();
-	}*/
-
-return 0;
+int main() {
+  printf("Hello, ysyx!\n");
+  return 0;
 }
