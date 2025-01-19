329b031649cdceb45d513ea7062ca9e407f059f0
tracer-ysyx <tracer@ysyx.org>
2025-01-19 21:47:18 +0800

    > compile NEMU ysyx_100012104 龙文庆 Linux long-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:18 up  3:19,  5 users,  load average: 0.13, 0.24, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -46,8 +46,8 @@ static struct rule {
 	{"-", TK_SUB},				// 减法
 	{"/", TK_DIVIDE},					// 除法
 	{"[0-9]+", TK_NUM},
-	{"[a-zA-Z]+", TK_ID}/*,
-	{"(", TK_L},
+	{"[a-zA-Z]+", TK_ID},
+	{"(", TK_L}/*,
 	{")", TK_R}*/
 };
 
