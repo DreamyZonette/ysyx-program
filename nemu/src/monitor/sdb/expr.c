1a2046b3cf257dd208377d59708dd2419de5b045
tracer-ysyx <tracer@ysyx.org>
2025-01-19 21:45:14 +0800

    > compile NEMU ysyx_100012104 龙文庆 Linux long-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:14 up  3:17,  5 users,  load average: 0.30, 0.28, 0.27

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -44,11 +44,11 @@ static struct rule {
   {"==", TK_EQ},        // equal
 	{"\\*", TK_MULTIPLY},      // multiplied
 	{"-", TK_SUB},				// 减法
-	{"/", TK_DIVIDE},					// 除法
+	{"/", TK_DIVIDE}/*,					// 除法
 	{"[0-9]+", TK_NUM},
 	{"[a-zA-Z]+", TK_ID},
 	{"(", TK_L},
-	{")", TK_R}
+	{")", TK_R}*/
 };
 
 #define NR_REGEX ARRLEN(rules)
