2058de4baf38e9da3f44a59aec05a377a49e9b7e
tracer-ysyx <tracer@ysyx.org>
2025-03-31 19:40:51 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:51 up  4:36,  1 user,  load average: 0.31, 0.55, 0.61

--- npc/vsrc/ysyx_25020042_rom.v
+++ npc/vsrc/ysyx_25020042_rom.v
@@ -3,9 +3,22 @@ module ysyx_25020042_rom # (ADDR_WIDTH = 32)(
     output [31:0] data
 );
 
-    reg [31:0] rom_mem [80000000:80000000+1024-1] = '{default:0};
+    // ROM 定义：1024 项，每项 32 位（4 字节）
+    reg [31:0] rom_mem [0:1023] = '{default:0};
 
-    assign data = rom_mem[addr];
+    // 地址转换为索引（按 4 字节对齐）
+    wire [31:0] rom_offset = (addr - 32'h80000000) >> 2;
+    // reg [31:0] rom_mem [80000000+1024-1:80000000] = '{default:0};
+    // initial begin
+    //     $readmemh("npc/rom.txt", rom_mem);
+    // end
+    initial begin
+        rom_mem[0] = 32'h00500093; // ADDI x1, x0, 5   # x1 = x0 + 5 = 0 + 5 = 5
+        rom_mem[1] = 32'hffd08113; // ADDI x2, x1, -3  # x2 = x1 + (-3) = x1 - 3
+    end
+    // initial begin
+
+    assign data = rom_mem[rom_offset];
 
     
 
