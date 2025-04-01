0c9627a9f75d966de5bf85f66cd043f50bf37d77
tracer-ysyx <tracer@ysyx.org>
2025-04-01 15:30:54 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:54 up 19 min,  1 user,  load average: 1.34, 0.95, 0.72

--- npc/obj_dir/Vtop__Trace__0.cpp
+++ npc/obj_dir/Vtop__Trace__0.cpp
@@ -315,15 +315,12 @@ void Vtop___024root__trace_chg_sub_0(Vtop___024root* vlSelf, VerilatedVcd::Buffe
                                     (0x1fU & (vlSelf->top__DOT__data 
                                               >> 7U))) 
                                    >> 9U))));
-        bufp->chgIData(oldp+110,(((vlSelf->top__DOT__addr 
-                                   - (IData)(0x80000000U)) 
-                                  >> 2U)),32);
-        bufp->chgSData(oldp+111,((0x3ffU & ((vlSelf->top__DOT__addr 
+        bufp->chgSData(oldp+110,((0x3ffU & ((vlSelf->top__DOT__addr 
                                              - (IData)(0x80000000U)) 
                                             >> 2U))),10);
     }
-    bufp->chgBit(oldp+112,(vlSelf->clk));
-    bufp->chgBit(oldp+113,(vlSelf->reset));
+    bufp->chgBit(oldp+111,(vlSelf->clk));
+    bufp->chgBit(oldp+112,(vlSelf->reset));
 }
 
 void Vtop___024root__trace_cleanup(void* voidSelf, VerilatedVcd* /*unused*/) {
--- npc/vsrc/ysyx_25020042_rom.v
+++ npc/vsrc/ysyx_25020042_rom.v
@@ -1,15 +1,15 @@
 module ysyx_25020042_rom # (ADDR_WIDTH = 32)(
+    input clk,
     input [ADDR_WIDTH-1:0] addr,// pc
-    output [31:0] data
+    output reg [31:0] data
 );
 
     // ROM 定义：1024 项，每项 32 位（4 字节）
-    reg [31:0] rom_mem [0:1023] = '{default:0};
+    reg [31:0] rom_mem [0:1023];
 
     // 地址转换为索引（按 4 字节对齐）
-    // wire [31:0] shifted_addr = (addr - 32'h80000000) >> 2;
-    // wire [9:0] rom_offset = shifted_addr[9:0];
-    wire [9:0] rom_offset = ((addr - 32'h80000000) >> 2) & 10'h3FF; // 用掩码取低10位
+    wire [31:0] shifted_addr = (addr - 32'h80000000) >> 2;
+    wire [9:0] rom_offset = shifted_addr[9:0];
     // reg [31:0] rom_mem [80000000+1024-1:80000000] = '{default:0};
     // initial begin
     //     $readmemh("npc/rom.txt", rom_mem);
@@ -19,8 +19,10 @@ module ysyx_25020042_rom # (ADDR_WIDTH = 32)(
         rom_mem[1] = 32'hffd08113; // ADDI x2, x1, -3  # x2 = x1 + (-3) = x1 - 3
     end
     // initial begin
-
-    assign data = rom_mem[rom_offset];
+    awlays @(posedge clk) begin
+        data <= rom_mem[rom_offset];
+    end
+    //assign data = rom_mem[rom_offset];
 
     
 
