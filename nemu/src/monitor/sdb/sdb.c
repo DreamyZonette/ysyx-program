5b8c7729c0602c00b1efbb7a53d6a1be5d8030d7
tracer-ysyx <tracer@ysyx.org>
2025-04-01 16:46:10 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:46:10 up  1:34,  1 user,  load average: 1.46, 1.24, 0.98

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -114,22 +114,22 @@ static int cmd_x (char *args){
 		return 0;
 	} 
 //获 得地址	
-	// char *endptr;
-	// char* addr_str = strtok(NULL, delim);
-	// long addr = strtol(addr_str, &endptr, 16);
-	// if (*endptr == '\0') {
-	// printf("addr: %lx\n",addr);
-	// } 
-	// else {
-	// 	printf("Parsed hex string. Stopped at: %s\n", endptr);
-	// } 
+	char *endptr;
 	char* addr_str = strtok(NULL, delim);
-	uint32_t addr = expr(addr_str, 0);
-	if (addr < 0x80000000) {
-		printf("Invalid address\n");
-		printf("Address is out of range\n");
-		return 0;
-	}
+	uint32_t addr = strtol(addr_str, &endptr, 16);
+	if (*endptr == '\0') {
+	printf("addr: 0x%x\n",addr);
+	} 
+	else {
+		printf("Parsed hex string. Stopped at: %s\n", endptr);
+	} 
+	// char* addr_str = strtok(NULL, delim);
+	// uint32_t addr = expr(addr_str, 0);
+	// if (addr < 0x80000000) {
+	// 	printf("Invalid address\n");
+	// 	printf("Address is out of range\n");
+	// 	return 0;
+	// }
 	for(int i = 0 ; i < N  ; i++){
 		printf("addr:0x%x --> %x\n",addr,paddr_read(addr,4));
 		addr += 4;
