1f9c1bea9ebdcf402499ccd7b05431cefe3cd712
tracer-ysyx <tracer@ysyx.org>
2025-05-26 22:47:21 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:47:21 up  6:05,  2 users,  load average: 1.53, 1.25, 1.16

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,18 +51,25 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) {
   #ifdef CONFIG_MTRACE
-
+    char p [128];
+    snprintf(p, 127, "read:"FMT_WORD"\tlen:%d", addr, len);
+    p[127] = '\0';
+    log_write("%s\n", p);
   #endif
-  return pmem_read(addr, len);
-  }
+  if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+    char p [128];
+    snprintf(p, 127, "write:"FMT_WORD"\tlen:%d\tdata:%u", addr, len, data);
+    p[127] = '\0';
+    log_write("%s\n", p);
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
