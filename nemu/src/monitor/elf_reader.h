fa10bd62ceff3169d0290875c894155905ee6d41
tracer-ysyx <tracer@ysyx.org>
2025-06-08 11:51:00 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:51:00 up  1:51,  1 user,  load average: 0.90, 0.81, 0.74

--- nemu/src/monitor/elf_reader.h
+++ nemu/src/monitor/elf_reader.h
@@ -6,11 +6,11 @@
 // 函数表结构定义
 typedef struct functab {
     uint32_t value;
-    char func_name[20];
+    char func_name[32];
 } Functab;
 
 // 全局函数表（外部可访问）
-extern Functab functab[30];
+extern Functab* functab;
 
 // 全局函数计数（外部可访问）
 extern int functab_count;
