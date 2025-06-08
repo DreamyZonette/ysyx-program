f7928e265685283b2a514823791ad37d0e724869
tracer-ysyx <tracer@ysyx.org>
2025-06-08 22:41:52 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:52 up  9:35,  1 user,  load average: 1.36, 1.07, 0.93

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -141,7 +141,13 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 1: img_file = optarg; return 0;
+      case 1: 
+        img_file = optarg; 
+        #ifndef CONFIG_FTRACE 
+          printf("test\n");
+          extract_functions(img_file);
+        #endif
+        return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
@@ -152,10 +158,7 @@ static int parse_args(int argc, char *argv[]) {
         exit(0);
     }
   }
-#ifndef CONFIG_FTRACE 
-  printf("test\n");
-	extract_functions(img_file);
-#endif
+
   return 0;
 }
 
