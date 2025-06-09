ecafba87c3d42490228aac2e86547819380ae250
tracer-ysyx <tracer@ysyx.org>
2025-06-09 22:12:00 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:00 up  2:01,  1 user,  load average: 1.19, 0.94, 1.00

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -23,7 +23,10 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     uint32_t ref_value = ref_r->gpr[i];
     uint32_t value = gpr(i);
     // printf("local:0x%08x\tref:0x%08x\n", value, ref_value);
-    if(value != ref_value) return false;
+    if(value == ref_value) {
+    printf("\33[1;31mlocal:0x%08x[0m] \33[1;32mref:0x%08x[0m\n", value, ref_value);
+    return false;
+    }
   }
   
   //if(value == ref_value) return true;
