5ff958892326dc6d9c3659248cb152cf22ab4024
tracer-ysyx <tracer@ysyx.org>
2025-07-20 11:34:30 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:30 up  1:36,  1 user,  load average: 2.04, 1.24, 0.97

--- npc/vsrc/EXU/alu.v
+++ npc/vsrc/EXU/alu.v
@@ -1,26 +1,26 @@
-/* verilator lint_off DECLFILENAME */
-module alu #(WIDTH = 32)(
-    input [WIDTH-1:0] i_src1,
-    input [WIDTH-1:0] i_src2,
-    input [WIDTH-1:0] i_imm ,
-    input [WIDTH-1:0] i_offset ,
-    input [WIDTH-1:0] i_ram_data,
-    input             i_addi_signal,
-    input             i_jalr_signal,
-    output reg [WIDTH-1:0] o_result,
-    );
+// /* verilator lint_off DECLFILENAME */
+// module alu #(WIDTH = 32)(
+//     input [WIDTH-1:0] i_src1,
+//     input [WIDTH-1:0] i_src2,
+//     input [WIDTH-1:0] i_imm ,
+//     input [WIDTH-1:0] i_offset ,
+//     input [WIDTH-1:0] i_ram_data,
+//     input             i_addi_signal,
+//     input             i_jalr_signal,
+//     output reg [WIDTH-1:0] o_result,
+//     );
 
-    always @ (*) begin
-        o_result = 32'b0;
-        if(i_addi_signal == 1'b1) begin
-            o_result = i_src1 + i_imm;
-        end
-        if(i_jalr_signal == 1'b1) begin
-            o_result = i_src1 + i_offset;
-        end
-    end
-    //临时
-    assign pc_next = 0;
+//     always @ (*) begin
+//         o_result = 32'b0;
+//         if(i_addi_signal == 1'b1) begin
+//             o_result = i_src1 + i_imm;
+//         end
+//         if(i_jalr_signal == 1'b1) begin
+//             o_result = i_src1 + i_offset;
+//         end
+//     end
+//     //临时
+//     assign pc_next = 0;
     
-endmodule
-/* verilator lint_off DECLFILENAME */
+// endmodule
+// /* verilator lint_off DECLFILENAME */
