2c3fd0168fb85f3147bc2b7e2f57016e302b68d0
tracer-ysyx <tracer@ysyx.org>
2025-07-21 20:35:52 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:52 up 16:25,  1 user,  load average: 0.58, 0.48, 0.48

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]){
     printf("Simulation start\n");
     top->sys_rst_n = 1;
     top->eval();
-    while(!sim_finish && count < 1) {
+    while(!sim_finish && count <= 1) {
         count ++;
         printf("%d: pc:%08x    inst:%08x   halt:%d\n", count, top->de_pc, top->de_inst, top->halt);
         //printf("1\n");
