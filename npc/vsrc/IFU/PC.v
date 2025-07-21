9091c973cbf29475cc08a35a8abc13034bf94b48
tracer-ysyx <tracer@ysyx.org>
2025-07-21 20:16:45 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:45 up 16:06,  1 user,  load average: 0.78, 0.83, 0.67

--- npc/vsrc/IFU/PC.v
+++ npc/vsrc/IFU/PC.v
@@ -3,7 +3,7 @@ module PC #(PC_LEN = 32)(
     input i_sys_clk,
     input i_sys_rst_n,
     input [PC_LEN-1:0] i_next_pc,
-    output reg [PC_LEN-1:0] o_pc = = 32'h80000000
+    output reg [PC_LEN-1:0] o_pc = 32'h80000000
     );
     
     always @(posedge i_sys_clk) begin
