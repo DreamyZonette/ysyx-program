23a4800ad5b249ea00d0d23798889040f7260b03
tracer-ysyx <tracer@ysyx.org>
2025-07-21 20:13:42 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:42 up 16:03,  1 user,  load average: 1.53, 0.93, 0.66

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -51,10 +51,10 @@ int main(int argc, char *argv[]){
     init_monitor(argc, argv);
 
     int count = 0;
-    // top->sys_clk = 0;
-    // top->sys_rst_n = 0;
-    // top->sys_clk = 1;
-    // top->sys_rst_n = 1;
+    top->sys_clk = 0;
+    top->sys_rst_n = 0;
+    top->sys_clk = 1;
+    top->sys_rst_n = 1;
     top->eval();
     while(!sim_finish && count < 1000) {
         count ++;
