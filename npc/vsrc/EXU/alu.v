4267643a4bc4562a76fe6b71c89d3e52cc760bea
tracer-ysyx <tracer@ysyx.org>
2025-07-22 13:47:09 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:47:09 up 22:50,  1 user,  load average: 1.16, 1.04, 1.02

--- npc/vsrc/EXU/alu.v
+++ npc/vsrc/EXU/alu.v
@@ -152,11 +152,11 @@ module alu #(WIDTH = 32)(
             o_data = i_pc_data + i_offset;
         // S型
         end else if(i_sw_signal == 1'b1) begin
-            o_data = i_src2;
+            o_data = i_src1 + i_offset;
         end else if(i_sh_signal == 1'b1) begin
-            o_data = i_src2;
+            o_data = i_src1 + i_offset;
         end else if(i_sb_signal == 1'b1) begin
-            o_data = i_src2;
+            o_data = i_src1 + i_offset;
         end 
         else begin
             o_halt_signal = 1'b1;
