50616c68415b3d04658214583ba2ff2c22529901
tracer-ysyx <tracer@ysyx.org>
2025-07-22 18:50:01 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:50:01 up 1 day,  3:53,  1 user,  load average: 1.05, 1.21, 1.23

--- npc/csrc/monitor/sdb/sdb.c
+++ npc/csrc/monitor/sdb/sdb.c
@@ -59,7 +59,7 @@ static int cmd_info(char *args){
 	char* Arg = strtok(args," ");
 	if(strcmp( Arg , "r") == 0){
 		//isa_reg_display();	
-        printf("0x%o8x\n", top->IDU_u->gpr_u->reg_file[2]);
+        printf("0x%o8x\n", top->IDU->gpr->reg_file[2]);
 	}
 	else {
 	printf("Invalid argument");
