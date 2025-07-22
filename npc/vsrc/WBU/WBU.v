a4a85eadedd38edc2329011a53d7b0a7012620e3
tracer-ysyx <tracer@ysyx.org>
2025-07-22 11:34:39 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:39 up 20:38,  1 user,  load average: 0.40, 0.32, 0.25

--- npc/vsrc/WBU/WBU.v
+++ npc/vsrc/WBU/WBU.v
@@ -26,7 +26,7 @@ module WBU(
 
     always @(*) begin
         if (i_load_signal == 1'b1) begin
-            reg_wdata = i_sys_wdata;
+            reg_wdata = i_load_wdata;
         end else if (i_jal_signal == 1'b1 || i_jalr_signal == 1'b1)begin
             reg_wdata = i_cur_pc + 4;
         end
