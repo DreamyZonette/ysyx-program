d08d1dd652ec70f789f3c970f6cc386c7e0362f3
tracer-ysyx <tracer@ysyx.org>
2025-07-22 16:36:21 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:21 up 1 day,  1:40,  1 user,  load average: 2.04, 1.52, 1.27

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -56,7 +56,7 @@ void cpu_exec(uint64_t n){
     case NPC_RUNNING: npc_state.state = NPC_STOP; break;
 
     case NPC_END: case NPC_ABORT:
-      printf("npc: %s at pc = 0x08x",
+      printf("npc: %s at pc = 0x%08x\n",
           (npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
