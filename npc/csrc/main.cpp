a3ae406b2957d05d9582729e3766a9017dee5cfd
tracer-ysyx <tracer@ysyx.org>
2025-07-22 16:42:23 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:23 up 1 day,  1:46,  1 user,  load average: 2.01, 1.59, 1.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -9,7 +9,7 @@ extern void init_isa();
 void init_monitor(int, char *[]);
 void step_and_dump_wave();
 // void sim_run();
-extern void engine_start();
+void engine_start();
 
 VerilatedContext* contextp;
 VerilatedVcdC* tfp;
