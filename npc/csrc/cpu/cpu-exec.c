54418451258c93005d6418d7fb1bbd3ecfdc27cb
tracer-ysyx <tracer@ysyx.org>
2025-07-22 15:10:46 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:10:46 up 1 day, 14 min,  1 user,  load average: 1.13, 1.31, 1.33

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -1,5 +1,16 @@
 #include <cpu/cpu.h>
 
+// 全局结束标志和 DPI-C 函数
+bool sim_finish = false;
+bool is_hit_good_trap = true;
+extern "C" void dpi_ebreak() {
+    is_hit_good_trap = false;
+    sim_finish = true;  // 触发仿真结束
+}
+extern "C" void dpi_return() {
+    sim_finish = true;  // 触发仿真结束
+}
+
 void step_and_dump_wave(){
     top->eval();
     contextp->timeInc(1);   
@@ -14,12 +25,32 @@ void single_cycle() {
 }
 
 void cpu_exec(uint64_t n){
-    single_cycle();
+    while(n--){
+        single_cycle();
+    }
+    
 }
 
 void sim_run(){
-    while(n--){
-        cpu_exec(n);
+    top->sys_rst_n = 0;
+    top->sys_clk = 0;
+    step_and_dump_wave();
+    top->sys_clk = 1;
+    step_and_dump_wave();
+    top->sys_rst_n = 1;
+    // top->sys_clk = 0;
+    // step_and_dump_wave();
+    // top->sys_clk = 1;
+    step_and_dump_wave();
+
+    while(!sim_finish && count <= 20) {
+        count ++;
+        //printf("%4d: pc:%08x    inst:%08x   halt:%d\n", count, top->de_pc, top->de_inst, top->halt);
+        //single_cycle();
+        cpu_exec(1);
+        if(top->halt == 1) is_hit_good_trap = false;
     }
+    if(is_hit_good_trap) printf("npc:%s at pc:0x%08x\n", ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN), top->de_pc);
+    else printf("npc:%s at pc:0x%08x\n", ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED), top->de_pc);
     
 }
