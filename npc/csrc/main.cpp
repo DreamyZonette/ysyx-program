bccc96ba54d0d1cdc3869d1b62d5a296fab4735f
tracer-ysyx <tracer@ysyx.org>
2025-07-22 12:59:49 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:59:49 up 22:03,  1 user,  load average: 1.86, 1.16, 0.57

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -60,6 +60,9 @@ int main(int argc, char *argv[]){
     // step_and_dump_wave();
     top->sys_rst_n = 1;
     top->sys_clk = 0;
+    step_and_dump_wave();
+    top->sys_clk = 1;
+    step_and_dump_wave();
     while(!sim_finish && count <= 10) {
         count ++;
         printf("%4d: pc:%08x    inst:%08x   halt:%d\n", count, top->de_pc, top->de_inst, top->halt);
