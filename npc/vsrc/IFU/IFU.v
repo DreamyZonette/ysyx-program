e9b96bcbfc783c2ae00fe2f42fd02eafe90fdac6
tracer-ysyx <tracer@ysyx.org>
2025-07-22 13:27:02 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:27:02 up 22:30,  1 user,  load average: 0.81, 0.90, 0.96

--- npc/vsrc/IFU/IFU.v
+++ npc/vsrc/IFU/IFU.v
@@ -4,7 +4,7 @@ module IFU(
 );
 import "DPI-C" function int pmem_read(input int raddr, input int len);
 
-assign o_instruction = pmem_read(i_pc, 4);
+assign o_instruction = $unsigned(pmem_read(i_pc, 4)); 
 
 
 
