4c16cbba7755f47928e9aee4b96d959c5ce1a5f6
tracer-ysyx <tracer@ysyx.org>
2025-07-24 16:08:12 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:12 up  4:26,  1 user,  load average: 0.65, 0.47, 0.47

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -37,8 +37,8 @@ void npc_engine_start() {
     top->sys_rst_n = 1;
     step_and_dump_wave();
     
-    // top->sys_clk = 1;
-    // step_and_dump_wave();
+    top->sys_clk = 1;
+    step_and_dump_wave();
     //printf("NPC start at pc:0x%08x\tinstruction:%0x%08x\n", top->de_pc, top->de_inst);
 }
 
