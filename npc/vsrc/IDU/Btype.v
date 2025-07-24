86d2a46feecb8a942f4ee5a46e4e322224ec226b
tracer-ysyx <tracer@ysyx.org>
2025-07-24 19:26:59 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:59 up  7:44,  1 user,  load average: 0.22, 0.24, 0.32

--- npc/vsrc/IDU/Btype.v
+++ npc/vsrc/IDU/Btype.v
@@ -45,10 +45,10 @@ module Btype (
                 o_bgeu_signal = (fun1 == 3'b111) ? 1'b1 : 1'b0;
                 o_blt_signal  = (fun1 == 3'b100) ? 1'b1 : 1'b0;
                 o_bltu_signal = (fun1 == 3'b110) ? 1'b1 : 1'b0;
-                if(fun1 == 3'b000 || fun1 == 3'b101 || fun1 == 3'b001 || fun1 == 3'b100) begin
+                if(fun1 == 3'b000 || fun1 == 3'b101 || fun1 == 3'b001 || fun1 == 3'b100 || fun1 == 3'b110 || fun1 == 3'b111) begin
                     sign_extended = 1'b1;
-                end else if(fun1 == 3'b111 || fun1 == 3'b110) begin
-                    zero_extended = 1'b1;
+                // end else if(fun1 == 3'b111) begin
+                //     zero_extended = 1'b1;
                 end else begin
                     zero_extended = 1'b1;
                 end
