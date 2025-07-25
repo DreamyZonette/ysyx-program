b9e8d4e69324d01ea76df92461d1dfa15139d989
tracer-ysyx <tracer@ysyx.org>
2025-07-25 21:18:24 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:18:24 up  4:33,  1 user,  load average: 0.40, 0.65, 0.48

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -62,7 +62,7 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  // panic("Not implemented");
+  panic("Not implemented");
   if (size == 0) return NULL;
   if (free_list == (mem_block_t *)memory_pool && free_list->size == 0) {
     init_memory_pool();
