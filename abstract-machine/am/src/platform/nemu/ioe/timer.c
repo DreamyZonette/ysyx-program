20275b43e53671fb4404c02abb428ea9c2bc6d8b
tracer-ysyx <tracer@ysyx.org>
2025-07-25 16:41:10 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:10 up  1:21,  1 user,  load average: 0.48, 0.37, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,12 +2,25 @@
 #include <nemu.h>
 #include <klib.h>
 
+static uint64_t last_counter = 0;
+
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = cpu_count();
-  //printf("%d\n", uptime->us);
+  uint64_t counter = cpu_count();
+  uint64_t diff = 0;
+  if (counter < last_counter) {
+    // overflow
+    counter += 0x100000000;
+  }
+  uptime->us = (counter - last_counter) * 1000000 / cpu_count();
+  if(last_counter != counter) diff = 1;
+  last_counter = counter;
+  if(diff == 1){
+
+  printf("%d\n", uptime->us);
+  }
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
