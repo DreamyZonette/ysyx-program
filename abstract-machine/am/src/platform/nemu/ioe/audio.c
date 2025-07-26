a6b5f412e28683b5ea1094257b17353a7d3375a3
tracer-ysyx <tracer@ysyx.org>
2025-07-26 20:51:55 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:55 up  4:06,  1 user,  load average: 0.34, 0.60, 0.63

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -26,7 +26,7 @@ void __am_audio_init() {
   // audio_freq = inl(AUDIO_FREQ_ADDR);
   // audio_channels = inl(AUDIO_CHANNELS_ADDR);
   // audio_samples = inl(AUDIO_SAMPLES_ADDR);
-  //audio_sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+  audio_sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
   //printf("%d\n", audio_sbuf_size);
 
   if (audio_sbuf_size > AUDIO_BUF_SIZE) {
