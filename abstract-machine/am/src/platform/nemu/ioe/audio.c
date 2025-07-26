4d7e5fe2a0d8b034c0e4af580506314223a513ff
tracer-ysyx <tracer@ysyx.org>
2025-07-26 19:06:09 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:06:09 up  2:20,  1 user,  load average: 0.07, 0.18, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,47 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+#define AUDIO_BUF_SIZE 4096
+
+static uint32_t audio_freq = 44100;
+static uint32_t audio_channels = 2;
+static uint32_t audio_samples = 1024;
+static uint32_t audio_sbuf_size = AUDIO_BUF_SIZE;
+static uint8_t  audio_sbuf[AUDIO_BUF_SIZE];
+static uint32_t audio_write_pos = 0; 
+
+
+
 void __am_audio_init() {
+  outl(AUDIO_INIT_ADDR, 1);
+
+  audio_freq = inl(AUDIO_FREQ_ADDR);
+  audio_channels = inl(AUDIO_CHANNELS_ADDR);
+  audio_samples = inl(AUDIO_SAMPLES_ADDR);
+  audio_sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+
+  if (audio_sbuf_size > AUDIO_BUF_SIZE) {
+    audio_sbuf_size = AUDIO_BUF_SIZE;
+  }
+
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  if(cfg->bufsize == 0)
+  cfg->bufsize = audio_sbuf_size;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  ctrl->freq = audio_freq;
+  ctrl->channels = audio_channels;
+  ctrl->samples = audio_samples;
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  Area area = ctl->buf;
 }
