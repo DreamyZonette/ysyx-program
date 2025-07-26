e0a1c960a5b62af87eaa3e8be1fdd8d70a9bba9b
tracer-ysyx <tracer@ysyx.org>
2025-07-26 13:32:17 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:32:17 up  9:20,  1 user,  load average: 1.37, 0.84, 0.74

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -36,7 +36,7 @@ void vga_update_screen();
 void device_update() {
   static uint64_t last = 0;
   uint64_t now = get_time();
-  printf("1\n");
+  //printf("1\n");
   if (now - last < 1000000 / TIMER_HZ) {
     return;
   }
