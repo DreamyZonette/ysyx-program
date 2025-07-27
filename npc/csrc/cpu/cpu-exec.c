38677e7aab19328c3dd012ddafba2e5a4f811f5d
tracer-ysyx <tracer@ysyx.org>
2025-07-27 19:43:23 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:43:23 up 14:18,  1 user,  load average: 0.39, 0.33, 0.52

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -109,8 +109,6 @@ static void statistic() {
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
-
-if(npc_state.state == NPC_ABORT) assert(0);
 }
 
 void assert_fail_msg() {
