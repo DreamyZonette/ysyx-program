c05e4ab2ad8cad66cddb58e35349d8908a4e827a
tracer-ysyx <tracer@ysyx.org>
2025-07-27 19:07:01 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:07:01 up 13:42,  1 user,  load average: 1.40, 1.29, 1.11

--- npc/csrc/utils/timer.c
+++ npc/csrc/utils/timer.c
@@ -19,7 +19,7 @@ static uint64_t boot_time = 0;
 static uint64_t get_time_internal() {
 #if defined(CONFIG_TARGET_AM)
   uint64_t us = io_read(AM_TIMER_UPTIME).us;
-  //printf("%lx\n", us);
+  printf("%lx\n", us);
 #elif defined(CONFIG_TIMER_GETTIMEOFDAY)
   struct timeval now;
   gettimeofday(&now, NULL);
@@ -29,7 +29,7 @@ static uint64_t get_time_internal() {
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
-  printf("%lx\n", us);
+  // printf("%lx\n", us);
 #endif
   return us;
 }
