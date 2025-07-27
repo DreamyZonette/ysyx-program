677ed04815c8e3962352533d7c6f1f52e8d54e2f
tracer-ysyx <tracer@ysyx.org>
2025-07-27 14:50:13 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:13 up 10:15,  1 user,  load average: 1.09, 1.14, 1.07

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -100,6 +100,17 @@ void single_cycle() {
   step_and_dump_wave();
 }
 
+static void statistic() {
+//   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
+// #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
+//   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
+//   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
+//   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
+//   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+
+if(npc_state.state == NPC_ABORT) assert(0);
+}
+
 void assert_fail_msg() {
 #ifdef CONFIG_IRINGBUF
   print_iringbuf(cpu.pc);
@@ -146,16 +157,7 @@ static void execute(uint64_t n) {
   }
 }
 
-static void statistic() {
-//   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
-// #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
-//   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
-//   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
-//   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
-//   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
 
-if(npc_state.state == NPC_ABORT) assert(0);
-}
 
 void cpu_exec(uint64_t n){
     if(sim_finish) npc_state.state = NPC_END;
