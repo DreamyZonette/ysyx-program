f571f5ac90c7102e14dbaf0c9f05bd34394dcc98
tracer-ysyx <tracer@ysyx.org>
2025-07-27 14:51:39 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:39 up 10:16,  1 user,  load average: 0.81, 1.08, 1.06

--- npc/csrc/device/io/map.c
+++ npc/csrc/device/io/map.c
@@ -18,7 +18,7 @@ uint8_t* new_space(int size) {
 
 static void check_bound(IOMap *map, paddr_t addr) {
   if (map == NULL) {
-    Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, top_de_pc);
+    Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, top->de_pc);
   } else {
     Assert(addr <= map->high && addr >= map->low,
         "address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
