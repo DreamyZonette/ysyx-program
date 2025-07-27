856a2ab75c66331d74c8b384e60b8e8ab27da7ae
tracer-ysyx <tracer@ysyx.org>
2025-07-27 15:08:30 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:30 up 10:33,  1 user,  load average: 0.37, 0.40, 0.59

--- npc/include/cpu/difftest.h
+++ npc/include/cpu/difftest.h
@@ -26,14 +26,14 @@ void difftest_set_patch(void (*fn)(void *arg), void *arg);
 void difftest_step(vaddr_t pc, vaddr_t npc);
 void difftest_detach();
 void difftest_attach();
-// #else
+#else
 static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
 static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
 static inline void difftest_detach() {}
 static inline void difftest_attach() {}
-//
+#endif
 
 extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
 extern void (*ref_difftest_regcpy)(void *dut, bool direction);
@@ -49,6 +49,5 @@ static inline bool difftest_check_reg(const char *name, vaddr_t pc, word_t ref, 
   }
   return true;
 }
-#endif
 
 #endif
--- npc/include/device/map.h
+++ npc/include/device/map.h
@@ -23,7 +23,9 @@ static inline int find_mapid_by_addr(IOMap *maps, int size, paddr_t addr) {
   int i;
   for (i = 0; i < size; i ++) {
     if (map_inside(maps + i, addr)) {
+      #if CONFIG_DIFFTEST
       difftest_skip_ref();
+      #endif
       return i;
     }
   }
