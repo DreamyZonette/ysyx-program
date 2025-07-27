6cfe416b7578416f3269eddadd4e18aaf12c07b7
tracer-ysyx <tracer@ysyx.org>
2025-07-27 15:53:50 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:50 up 11:18,  1 user,  load average: 1.02, 0.64, 0.47

--- npc/csrc/utils/timer.c
+++ npc/csrc/utils/timer.c
@@ -1,4 +1,7 @@
 #include <common.h>
+#include <sys/time.h>
+
+static uint64_t boot_time = 0;
 
 static uint64_t get_time_internal() {
 
@@ -6,6 +9,12 @@ static uint64_t get_time_internal() {
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
   return us;
+}
+
+uint64_t get_time() {
+  if (boot_time == 0) boot_time = get_time_internal();
+  uint64_t now = get_time_internal();
+  return now - boot_time;
 }
 
 void init_rand() {
