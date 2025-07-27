feee5c82225a4a4fc50a2826c03a2f738cf539df
tracer-ysyx <tracer@ysyx.org>
2025-07-27 20:13:49 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:49 up 14:48,  1 user,  load average: 0.41, 0.57, 0.56

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -152,7 +152,7 @@ static void execute(uint64_t n) {
     if(top->halt == 1){
       npc_state.halt_pc = top->de_pc;
       npc_state.halt_ret = top->reg_data[10];
-      npc_state.state = NPC_ABORT;
+      // npc_state.state = NPC_ABORT;
     }
 
     if (npc_state.state != NPC_RUNNING) break;
