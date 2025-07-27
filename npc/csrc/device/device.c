e2000369bb665e96b0789737b29755454d5e813a
tracer-ysyx <tracer@ysyx.org>
2025-07-27 16:08:36 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:36 up 11:33,  1 user,  load average: 1.32, 1.17, 0.85

--- npc/csrc/device/device.c
+++ npc/csrc/device/device.c
@@ -69,13 +69,27 @@ void init_device() {
   IFDEF(CONFIG_TARGET_AM, ioe_init());
   init_map();
 
-  IFDEF(CONFIG_HAS_SERIAL, init_serial());
-  IFDEF(CONFIG_HAS_TIMER, init_timer());
-  IFDEF(CONFIG_HAS_VGA, init_vga());
-  IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
-  IFDEF(CONFIG_HAS_AUDIO, init_audio());
-  IFDEF(CONFIG_HAS_DISK, init_disk());
-  IFDEF(CONFIG_HAS_SDCARD, init_sdcard());
+  #if CONFIG_HAS_SERIAL 
+    init_serial(); 
+  #endif
+  #if CONFIG_HAS_TIMER
+    init_timer();
+  #endif
+  #if CONFIG_HAS_VGA
+    init_vga();
+  #endif
+  #if CONFIG_HAS_KEYBOARD
+    init_i8042();
+  #endif
+  #if CONFIG_HAS_AUDIO
+    init_audio();
+  #endif
+  #if CONFIG_HAS_DISK
+    init_disk();
+  #endif
+  #if CONFIG_HAS_SDCARD
+    init_sdcard();
+  #endif
 
   IFNDEF(CONFIG_TARGET_AM, init_alarm());
 }
