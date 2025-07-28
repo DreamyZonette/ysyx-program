a26cf57d68031a43620512571018accba9634d70
tracer-ysyx <tracer@ysyx.org>
2025-07-28 14:06:38 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:38 up  4:05,  1 user,  load average: 0.95, 0.69, 0.54

--- npc/csrc/momery/paddr.c
+++ npc/csrc/momery/paddr.c
@@ -36,6 +36,12 @@ static void out_of_bound(paddr_t addr) {
 
 extern "C" int pmem_read(int addr, int len) {
   addr = paddr_t(addr);
+  int offset = 0;
+  if(addr % 4 != 0){
+    offset = address % 4;
+    addr -= offset;
+    len += offset;
+  }
   uint32_t ret;
   if (addr == RTC_LO_ADDR || addr == RTC_HI_ADDR) { 
     ret = mmio_read(addr, len);
@@ -46,16 +52,17 @@ extern "C" int pmem_read(int addr, int len) {
       if (addr != top->de_pc)printf("DPI-RET: pmem_read(0x%08x, %d) = 0x%08x\n", addr, len, ret);
     #endif
   }
+  ret = ret >> (offset * 8);
   return ret;
 }
 
 extern "C" void pmem_write(int addr, int len, int data) {
   addr = paddr_t(addr);
   data = word_t(data);
-  uint32_t mask = 0;
-  if(len == 1) mask = 0xff;
-  else if(len == 2) mask = 0xffff;
-  else if(len == 4) mask = 0xffffffff;
+  // uint32_t mask = 0;
+  // if(len == 1) mask = 0xff;
+  // else if(len == 2) mask = 0xffff;
+  // else if(len == 4) mask = 0xffffffff;
   
   if(addr == SERIAL_PORT){
     // putchar(char(data));
