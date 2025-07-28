f810d8e1cbefb598873b2801bda4cf194012dddc
tracer-ysyx <tracer@ysyx.org>
2025-07-28 09:57:18 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:57:18 up 52 min,  1 user,  load average: 0.99, 1.02, 0.82

--- npc/csrc/device/device.c
+++ npc/csrc/device/device.c
@@ -9,7 +9,7 @@ void init_map();
 void init_serial();
 void init_timer();
 // void init_vga();
-// void init_i8042();
+void init_i8042();
 // void init_audio();
 // void init_disk();
 // void init_sdcard();
