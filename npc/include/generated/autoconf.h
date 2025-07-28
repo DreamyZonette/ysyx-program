d215c84f3652e9cb40551b0e3a2934b2e600ecdb
tracer-ysyx <tracer@ysyx.org>
2025-07-28 11:49:29 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:49:29 up  2:44,  1 user,  load average: 0.75, 0.41, 0.50

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 #define CONFIG_WAVE 0
 #define CONFIG_ITRACE 0
 #define CONFIG_MTRACE 0
-#define CONFIG_FTRACE 1
+#define CONFIG_FTRACE 0
 #define CONFIG_DTRACE 0
 #define CONFIG_TRACE 1
 #define CONFIG_DIFFTEST 0
