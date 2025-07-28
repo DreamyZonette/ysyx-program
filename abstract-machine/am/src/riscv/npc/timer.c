82b0dbe059bae810ca339b4eafff8ab6f182705a
tracer-ysyx <tracer@ysyx.org>
2025-07-28 09:45:26 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:45:26 up 40 min,  1 user,  load average: 0.71, 0.78, 0.68

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -26,39 +26,39 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = now - base_time;// (μs)
 }
 
-// static void convert_timestamp_to_calendar(uint64_t seconds, AM_TIMER_RTC_T *rtc) {
-//     // 简化计算：假设所有月份都是30天
-//     uint64_t secs = seconds;
+static void convert_timestamp_to_calendar(uint64_t seconds, AM_TIMER_RTC_T *rtc) {
+    // 简化计算：假设所有月份都是30天
+    uint64_t secs = seconds;
     
-//     rtc->second = secs % 60;
-//     secs /= 60;
-//     rtc->minute = secs % 60;
-//     secs /= 60;
-//     rtc->hour   = secs % 24;
-//     secs /= 24;
+    rtc->second = secs % 60;
+    secs /= 60;
+    rtc->minute = secs % 60;
+    secs /= 60;
+    rtc->hour   = secs % 24;
+    secs /= 24;
     
-//     // 计算年份（从1970年开始）
-//     rtc->year = 1970 + secs / 365;
-//     secs %= 365;
+    // 计算年份（从1970年开始）
+    rtc->year = 1970 + secs / 365;
+    secs %= 365;
     
-//     // 计算月份和日期
-//     rtc->month = secs / 30 + 1;
-//     secs %= 30;
-//     rtc->day = secs + 1;
-// }
+    // 计算月份和日期
+    rtc->month = secs / 30 + 1;
+    secs %= 30;
+    rtc->day = secs + 1;
+}
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-//  uint64_t elapsed_seconds = (am_get_time() - base_time) / 1000000;
-//     uint64_t current_seconds = base_rtc + elapsed_seconds;
+ uint64_t elapsed_seconds = (am_get_time() - base_time) / 1000000;
+    uint64_t current_seconds = base_rtc + elapsed_seconds;
     
-//     // 转换为日历时间
-//     convert_timestamp_to_calendar(current_seconds, rtc);
-  time_t now = time(NULL);
-  struct tm *utc_time = gmtime(&now);
-  rtc->second = utc_time->tm_sec;
-  rtc->minute = utc_time->tm_min;
-  rtc->hour   = utc_time->tm_hour;
-  rtc->day    = utc_time->tm_mday;
-  rtc->month  = utc_time->tm_mon + 1;    // tm_mon 范围是 0-11
-  rtc->year   = utc_time->tm_year + 1900; // tm_year 是从 1900 开始的年数
+    // 转换为日历时间
+    convert_timestamp_to_calendar(current_seconds, rtc);
+//   time_t now = time(NULL);
+//   struct tm *utc_time = gmtime(&now);
+//   rtc->second = utc_time->tm_sec;
+//   rtc->minute = utc_time->tm_min;
+//   rtc->hour   = utc_time->tm_hour;
+//   rtc->day    = utc_time->tm_mday;
+//   rtc->month  = utc_time->tm_mon + 1;    // tm_mon 范围是 0-11
+//   rtc->year   = utc_time->tm_year + 1900; // tm_year 是从 1900 开始的年数
 }
