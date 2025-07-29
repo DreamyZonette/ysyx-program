af229e8b627a28f05bc43e5be9a6c91aff8aa5a3
tracer-ysyx <tracer@ysyx.org>
2025-07-29 11:11:52 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:11:52 up  2:12,  1 user,  load average: 0.06, 0.17, 0.22

--- npc/vsrc/IDU/Stype.v
+++ npc/vsrc/IDU/Stype.v
@@ -6,6 +6,7 @@ module Stype (
     output reg [3:0] o_wmask,
     output reg o_sw_signal,
     output reg o_sb_signal,
+    output reg o_sh_signal,
     output o_halt_signal
 );
 
@@ -27,6 +28,7 @@ module Stype (
         // 初始化
         o_sw_signal     = 1'b0;
         o_sb_signal     = 1'b0;
+        o_sh_signal     = 1'b0;
         sign_extended   = 1'b0;
         zero_extended   = 1'b0;
         unknown_intstruction = 1'b0;
@@ -37,7 +39,8 @@ module Stype (
         7'b0100011: begin // store
             o_sw_signal = (fun == 3'b010) ? 1'b1 : 1'b0;
             o_sb_signal = (fun == 3'b000) ? 1'b1 : 1'b0;
-            sign_extended = (fun == 3'b010 || fun == 3'b000) ? 1'b1 : 1'b0;
+            o_sh_signal = (fun == 3'b001) ? 1'b1 : 1'b0;
+            sign_extended = (fun == 3'b010 || fun == 3'b000 || fun == 3'b001) ? 1'b1 : 1'b0;
         end
         default: begin
             unknown_intstruction = 1'b1;
@@ -60,6 +63,8 @@ module Stype (
             o_wmask = 4'd4;
         end else if (o_sb_signal == 1'b1) begin
             o_wmask = 4'd1;
+    end else if (o_sh_signal == 1'b1) begin
+            o_wmask = 4'd2;
         end else begin
             o_wmask = 4'd0;
         end
