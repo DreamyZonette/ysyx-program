d7c24a97755a9e81fdaafcfdf0eef0c3727b07d4
tracer-ysyx <tracer@ysyx.org>
2025-07-31 10:01:58 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:01:58 up 56 min,  1 user,  load average: 0.51, 0.55, 0.43

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -60,181 +60,181 @@ bool ioe_init() {
 void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
 void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
 
-//================gpu=================
+// //================gpu=================
 
-#define MMIO_BASE 0xa0000000
-#define DEVICE_BASE 0xa0000000
-#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
-#define SYNC_ADDR (VGACTL_ADDR + 4)
-#define FB_ADDR         (MMIO_BASE   + 0x1000000)
+// #define MMIO_BASE 0xa0000000
+// #define DEVICE_BASE 0xa0000000
+// #define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
+// #define SYNC_ADDR (VGACTL_ADDR + 4)
+// #define FB_ADDR         (MMIO_BASE   + 0x1000000)
 
-static uint32_t width = 0;
-static uint32_t height = 0;
+// static uint32_t width = 0;
+// static uint32_t height = 0;
 
-static void am_get_gpu_config() {
-    uint32_t config = inl(VGACTL_ADDR);
-    width = config >> 16;
-    height = config & 0xFFFF;
-}
+// static void am_get_gpu_config() {
+//     uint32_t config = inl(VGACTL_ADDR);
+//     width = config >> 16;
+//     height = config & 0xFFFF;
+// }
 
-void __am_gpu_init() {
-  am_get_gpu_config();
+// void __am_gpu_init() {
+//   am_get_gpu_config();
 
-  int i;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < width * height; i ++) fb[i] = 0;
-  outl(SYNC_ADDR, 1);
-}
+//   int i;
+//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//   for (i = 0; i < width * height; i ++) fb[i] = 0;
+//   outl(SYNC_ADDR, 1);
+// }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  if (width == 0 || height == 0) {
-    am_get_gpu_config();
-  }
+// void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+//   if (width == 0 || height == 0) {
+//     am_get_gpu_config();
+//   }
   
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = width, .height = height,
-    .vmemsz = width * height * sizeof(uint32_t)
-  };
-}
+//   *cfg = (AM_GPU_CONFIG_T) {
+//     .present = true, .has_accel = false,
+//     .width = width, .height = height,
+//     .vmemsz = width * height * sizeof(uint32_t)
+//   };
+// }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (width == 0 || height == 0) {
-        am_get_gpu_config();
-    }
-    if (ctl->x >= width || ctl->y >= height) return;
+// void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+//   if (width == 0 || height == 0) {
+//         am_get_gpu_config();
+//     }
+//     if (ctl->x >= width || ctl->y >= height) return;
 
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    uint32_t *pixels = (uint32_t *)ctl->pixels;
+//     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//     uint32_t *pixels = (uint32_t *)ctl->pixels;
 
-    uint32_t draw_w = ctl->w;
-    uint32_t draw_h = ctl->h;
+//     uint32_t draw_w = ctl->w;
+//     uint32_t draw_h = ctl->h;
 
-    if (ctl->x + draw_w > width) {
-      draw_w = width - ctl->x;
-    }
-    if (ctl->y + draw_h > height) {
-      draw_h = height - ctl->y;
-    }
+//     if (ctl->x + draw_w > width) {
+//       draw_w = width - ctl->x;
+//     }
+//     if (ctl->y + draw_h > height) {
+//       draw_h = height - ctl->y;
+//     }
 
-  //if (draw_w == 0 || draw_h == 0) return;
+//   //if (draw_w == 0 || draw_h == 0) return;
 
-  for (uint32_t y = 0; y < draw_h; y++) {
-    // 计算当前行在源和目标中的起始位置
-    uint32_t src_start = y * ctl->w;
-    uint32_t dst_start = (ctl->y + y) * width + ctl->x;
+//   for (uint32_t y = 0; y < draw_h; y++) {
+//     // 计算当前行在源和目标中的起始位置
+//     uint32_t src_start = y * ctl->w;
+//     uint32_t dst_start = (ctl->y + y) * width + ctl->x;
         
-    // 复制一行像素
-    for (uint32_t x = 0; x < draw_w; x++) {
-      fb[dst_start + x] = pixels[src_start + x];
-    }
-  }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
-}
+//     // 复制一行像素
+//     for (uint32_t x = 0; x < draw_w; x++) {
+//       fb[dst_start + x] = pixels[src_start + x];
+//     }
+//   }
+//   if (ctl->sync) {
+//     outl(SYNC_ADDR, 1);
+//   }
+// }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+// void __am_gpu_status(AM_GPU_STATUS_T *status) {
+//   status->ready = true;
+// }
 
-//================audio==================
-#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
-#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
-#define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
-#define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
-#define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
-#define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
-#define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
-#define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
-#define AUDIO_WP_ADDR     (AUDIO_ADDR + 0x18)
-#define AUDIO_RP_ADDR     (AUDIO_ADDR + 0x1c)
-#define AUDIO_BUF_SIZE 0x10000
-
-
-static uint32_t app_wp = 0;
+// //================audio==================
+// #define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
+// #define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
+// #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
+// #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
+// #define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
+// #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
+// #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
+// #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+// #define AUDIO_WP_ADDR     (AUDIO_ADDR + 0x18)
+// #define AUDIO_RP_ADDR     (AUDIO_ADDR + 0x1c)
+// #define AUDIO_BUF_SIZE 0x10000
 
 
+// static uint32_t app_wp = 0;
 
-void __am_audio_init() {
-  outl(AUDIO_INIT_ADDR, 1);
 
-  app_wp = 0;
-  outl(AUDIO_WP_ADDR, app_wp);
 
-}
+// void __am_audio_init() {
+//   outl(AUDIO_INIT_ADDR, 1);
 
-void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = true;
-  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
-}
+//   app_wp = 0;
+//   outl(AUDIO_WP_ADDR, app_wp);
 
-void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  if (ctrl->freq) {
-    outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  }
-  if (ctrl->channels) {
-    outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  }
-  if (ctrl->samples) {
-    outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  }
+// }
+
+// void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
+//   cfg->present = true;
+//   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+// }
+
+// void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+//   if (ctrl->freq) {
+//     outl(AUDIO_FREQ_ADDR, ctrl->freq);
+//   }
+//   if (ctrl->channels) {
+//     outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+//   }
+//   if (ctrl->samples) {
+//     outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+//   }
   
-}
+// }
 
-void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = inl(AUDIO_COUNT_ADDR);
-  //printf("%d\n", audio_count);
-  //outl(AUDIO_COUNT_ADDR, audio_count);
-}
+// void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
+//   stat->count = inl(AUDIO_COUNT_ADDR);
+//   //printf("%d\n", audio_count);
+//   //outl(AUDIO_COUNT_ADDR, audio_count);
+// }
 
-void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  Area *area = &ctl->buf;
-    uint8_t *src = (uint8_t *)area->start;
-    uint32_t len = (uint8_t *)area->end - src;
+// void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+//   Area *area = &ctl->buf;
+//     uint8_t *src = (uint8_t *)area->start;
+//     uint32_t len = (uint8_t *)area->end - src;
     
-    if (len == 0) {
-    // 更新写指针
-    outl(AUDIO_WP_ADDR, app_wp);
-    return;
-  }
+//     if (len == 0) {
+//     // 更新写指针
+//     outl(AUDIO_WP_ADDR, app_wp);
+//     return;
+//   }
     
-    // 获取当前缓冲区使用情况
-    uint32_t total_size = inl(AUDIO_SBUF_SIZE_ADDR);
-    uint32_t used = inl(AUDIO_COUNT_ADDR);
-    uint32_t free_space = total_size - used;
+//     // 获取当前缓冲区使用情况
+//     uint32_t total_size = inl(AUDIO_SBUF_SIZE_ADDR);
+//     uint32_t used = inl(AUDIO_COUNT_ADDR);
+//     uint32_t free_space = total_size - used;
 
-    if (len > free_space) {
-    len = free_space;
-  }
-    if (len == 0) return;
+//     if (len > free_space) {
+//     len = free_space;
+//   }
+//     if (len == 0) return;
     
-    uint8_t *hw_buf = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
-    if (app_wp + len <= total_size) {
-    // 情况1: 不需要回绕
-    for (uint32_t i = 0; i < len; i++) {
-      hw_buf[app_wp + i] = src[i];
-    }
-    app_wp += len;
-  } else {
-    // 情况2: 需要回绕
-    uint32_t part1 = total_size - app_wp;
-    for (uint32_t i = 0; i < part1; i++) {
-      hw_buf[app_wp + i] = src[i];
-    }
+//     uint8_t *hw_buf = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
+//     if (app_wp + len <= total_size) {
+//     // 情况1: 不需要回绕
+//     for (uint32_t i = 0; i < len; i++) {
+//       hw_buf[app_wp + i] = src[i];
+//     }
+//     app_wp += len;
+//   } else {
+//     // 情况2: 需要回绕
+//     uint32_t part1 = total_size - app_wp;
+//     for (uint32_t i = 0; i < part1; i++) {
+//       hw_buf[app_wp + i] = src[i];
+//     }
     
-    uint32_t part2 = len - part1;
+//     uint32_t part2 = len - part1;
 
-    for (uint32_t i = 0; i < part2; i++) {
-      hw_buf[i] = src[part1 + i];
-    }
-    app_wp = part2;
-  }
+//     for (uint32_t i = 0; i < part2; i++) {
+//       hw_buf[i] = src[part1 + i];
+//     }
+//     app_wp = part2;
+//   }
     
-    // 更新已使用空间计数
-  outl(AUDIO_WP_ADDR, app_wp);
+//     // 更新已使用空间计数
+//   outl(AUDIO_WP_ADDR, app_wp);
   
-  // 设置返回区域
-  ctl->buf.start = (void *)(hw_buf + app_wp);
-  ctl->buf.end = (void *)(hw_buf + app_wp + len);
-}
+//   // 设置返回区域
+//   ctl->buf.start = (void *)(hw_buf + app_wp);
+//   ctl->buf.end = (void *)(hw_buf + app_wp + len);
+// }
