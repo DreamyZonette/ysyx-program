1d3ab16147daf806d9aa30478bbe78de2f981e9f
tracer-ysyx <tracer@ysyx.org>
2025-08-01 09:14:58 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:14:58 up 11 min,  1 user,  load average: 0.21, 0.58, 0.38

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -50,7 +50,6 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   return true;
 }
 // struct Context {
-//   // TODO: fix the order of these members to match trap.S
 //   uintptr_t gpr[NR_REGS];
 //   uintptr_t mcause;
 //   uintptr_t mstatus;
