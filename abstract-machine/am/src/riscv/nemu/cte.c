6f7c33597b43eee1dca7b335056885bc801f1f57
tracer-ysyx <tracer@ysyx.org>
2025-08-01 10:44:33 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:44:33 up  1:41,  1 user,  load average: 0.38, 0.42, 0.50

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -63,13 +63,15 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   uintptr_t stack_top = (uintptr_t)(kstack.end);
   stack_top = stack_top & ~0xF;
-  Context *c = (Context*)(stack_top - sizeof(Context) + 32);
+  Context *c = (Context*)(stack_top - sizeof(Context));
   
   memset(c, 0, sizeof(Context));
   c->mepc = (uintptr_t)entry;
   c->gpr[10] = (uintptr_t)arg;
   c->gpr[2] = stack_top;
   c->mstatus = 0x1800;
+  printf("创建上下文: %d -> 大小=%d, 栈顶=%d\n", 
+       c, sizeof(Context), stack_top);
   return c;
 }
 
