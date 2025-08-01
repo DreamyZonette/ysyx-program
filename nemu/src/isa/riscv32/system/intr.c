69e70abc722c38ed708a5f869ff7fc496034ad55
tracer-ysyx <tracer@ysyx.org>
2025-08-01 20:52:15 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:15 up 10:00,  1 user,  load average: 0.26, 0.39, 0.41

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -23,7 +23,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
     epc+=4;
   }
   mcause = NO;
-  mepc = epc;
+  mepc = epc + 4;
 
   return mtvec;
 }
--- npc/vsrc/IDU/gpr.v
+++ npc/vsrc/IDU/gpr.v
@@ -18,27 +18,17 @@ module gpr  (
     assign o_reg_data = reg_file;
     assign wen = (i_rd != 5'b0)? (32'b1 << i_rd) : 32'b0; // 写使能信号
 
-    // // 写使能信号
-    // always @(*) begin   
-    //     if (i_rd != 5'b0) begin // x0 寄存器不可写
-    //         wen = (32'b1 << i_rd); // 直接左移生成 one-hot 信号
-    //     end
-    //     else begin
-    //         wen = 32'b0;
-    //     end
-    // end
-
     // 生成 32 个寄存器
-    Reg #(32, 32'b0) zero (i_sys_clk, i_sys_rst_n, i_data, reg_file[0], wen[0]);
-    Reg #(32, 32'b0) ra   (i_sys_clk, i_sys_rst_n, i_data, reg_file[1], wen[1]);
-    Reg #(32, 32'b0) sp   (i_sys_clk, i_sys_rst_n, i_data, reg_file[2], wen[2]);
-    Reg #(32, 32'b0) gp   (i_sys_clk, i_sys_rst_n, i_data, reg_file[3], wen[3]);
-    Reg #(32, 32'b0) tp   (i_sys_clk, i_sys_rst_n, i_data, reg_file[4], wen[4]);
-    Reg #(32, 32'b0) t0   (i_sys_clk, i_sys_rst_n, i_data, reg_file[5], wen[5]);
-    Reg #(32, 32'b0) t1   (i_sys_clk, i_sys_rst_n, i_data, reg_file[6], wen[6]);
-    Reg #(32, 32'b0) t2   (i_sys_clk, i_sys_rst_n, i_data, reg_file[7], wen[7]);
-    Reg #(32, 32'b0) s0   (i_sys_clk, i_sys_rst_n, i_data, reg_file[8], wen[8]);
-    Reg #(32, 32'b0) s1   (i_sys_clk, i_sys_rst_n, i_data, reg_file[9], wen[9]);
+    Reg #(32, 32'b0) zero (i_sys_clk, i_sys_rst_n, i_data, reg_file[0],  wen[0]);
+    Reg #(32, 32'b0) ra   (i_sys_clk, i_sys_rst_n, i_data, reg_file[1],  wen[1]);
+    Reg #(32, 32'b0) sp   (i_sys_clk, i_sys_rst_n, i_data, reg_file[2],  wen[2]);
+    Reg #(32, 32'b0) gp   (i_sys_clk, i_sys_rst_n, i_data, reg_file[3],  wen[3]);
+    Reg #(32, 32'b0) tp   (i_sys_clk, i_sys_rst_n, i_data, reg_file[4],  wen[4]);
+    Reg #(32, 32'b0) t0   (i_sys_clk, i_sys_rst_n, i_data, reg_file[5],  wen[5]);
+    Reg #(32, 32'b0) t1   (i_sys_clk, i_sys_rst_n, i_data, reg_file[6],  wen[6]);
+    Reg #(32, 32'b0) t2   (i_sys_clk, i_sys_rst_n, i_data, reg_file[7],  wen[7]);
+    Reg #(32, 32'b0) s0   (i_sys_clk, i_sys_rst_n, i_data, reg_file[8],  wen[8]);
+    Reg #(32, 32'b0) s1   (i_sys_clk, i_sys_rst_n, i_data, reg_file[9],  wen[9]);
     Reg #(32, 32'b0) a0   (i_sys_clk, i_sys_rst_n, i_data, reg_file[10], wen[10]);
     Reg #(32, 32'b0) a1   (i_sys_clk, i_sys_rst_n, i_data, reg_file[11], wen[11]);
     Reg #(32, 32'b0) a2   (i_sys_clk, i_sys_rst_n, i_data, reg_file[12], wen[12]);
@@ -60,20 +50,6 @@ module gpr  (
     Reg #(32, 32'b0) t4   (i_sys_clk, i_sys_rst_n, i_data, reg_file[29], wen[29]);
     Reg #(32, 32'b0) t5   (i_sys_clk, i_sys_rst_n, i_data, reg_file[30], wen[30]);
     Reg #(32, 32'b0) t6   (i_sys_clk, i_sys_rst_n, i_data, reg_file[31], wen[31]);
-
-    // genvar i;
-    // generate
-    //     for (i = 0; i < 32; i = i + 1) begin : register_instances
-    //         Reg #(32) Reg_u (
-    //             .clk(i_sys_clk),
-    //             .sys_rst_n(i_sys_rst_n),
-    //             .wen(wen [i]),
-    //             .i_data(i_data),
-    //             .o_data(reg_file[i])   
-    //         );
-    //     end
-    // endgenerate
-
 
 // 读取寄存器
     assign o_src1 = (i_rs1 == 5'b0)? 32'b0 : reg_file[i_rs1];
