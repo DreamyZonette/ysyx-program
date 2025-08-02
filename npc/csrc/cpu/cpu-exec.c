9f3aba505ae891975bdd3f8969c23564c751c183
tracer-ysyx <tracer@ysyx.org>
2025-08-02 13:58:30 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:58:30 up  2:59,  1 user,  load average: 2.75, 1.48, 0.93

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -162,8 +162,8 @@ static void execute(uint64_t n) {
     // dut.diff_mepc = top->de_mepc;
   #endif
     g_nr_guest_inst ++;
-    single_cycle();
     trace_and_difftest();
+    single_cycle();
     #if CONFIG_DEVICE
     device_update();
     #endif
