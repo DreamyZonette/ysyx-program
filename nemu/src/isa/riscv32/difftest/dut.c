cb430af72d6c2507a5c6148adc69bc00f198dbc1
tracer-ysyx <tracer@ysyx.org>
2025-08-02 16:08:34 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:34 up  1:17,  1 user,  load average: 1.71, 1.35, 0.92

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,6 +19,7 @@
 extern CPU_state cpu;
 
 bool skip = true;
+long long int count = 0;
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
@@ -33,7 +34,10 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   }
   if (skip == true){
     if(ref_r->mstatus == 0x1800) skip = false;
-    printf("skip\n");
+    count++;
+    if(count % 1000000 == 0) {
+      printf("skip: %lld\n", count);
+    }
   }
   else{
     if (cpu.mstatus != ref_r->mstatus){
