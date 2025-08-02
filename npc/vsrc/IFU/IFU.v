8233b10a08638162be49b51fadd3b09e51e23b44
tracer-ysyx <tracer@ysyx.org>
2025-08-02 18:06:53 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:53 up  3:16,  1 user,  load average: 0.36, 0.18, 0.18

--- npc/vsrc/IFU/IFU.v
+++ npc/vsrc/IFU/IFU.v
@@ -9,7 +9,7 @@ reg [31:0] instruction;
 // assign o_instruction = $unsigned(pmem_read(i_pc, 4)); 
 assign o_instruction = instruction; 
 
-always @(posedge i_sys_clk) begin
+always @(negedge i_sys_clk) begin
     instruction <= $unsigned(pmem_read(i_pc, 4)); 
 end
 
