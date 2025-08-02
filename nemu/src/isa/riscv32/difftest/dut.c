9ef0b044cd3c9279ddab4d4f4e6f5c492e419f21
tracer-ysyx <tracer@ysyx.org>
2025-08-02 15:58:38 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:38 up  1:07,  1 user,  load average: 1.13, 0.64, 0.63

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,6 +16,7 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
+extern CPU_state cpu;
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
@@ -27,6 +28,26 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     printf("gpr[%d] \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", i, value, ref_value);
     return false;
     }
+  }
+  if (pc != ref_r->pc) {
+    printf("pc \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", pc, ref_r->pc);
+    return false;
+  }
+  else if (cpu.mstatus != ref_r->mstatus){
+    printf("mstatus \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", cpu.mstatus, ref_r->mstatus);
+    return false;
+  }
+  else if (cpu.mepc != ref_r->mepc){
+    printf("mepc \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", cpu.mepc, ref_r->mepc);
+    return false;
+  }
+  else if (cpu.mcause != ref_r->mcause){
+    printf("mcause \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", cpu.mcause, ref_r->mcause);
+    return false;
+  }
+  else if (cpu.mtvec != ref_r->mtvec){
+    printf("mtvec \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", cpu.mtvec, ref_r->mtvec);
+    return false;
   }
   
   //if(value == ref_value) return true;
