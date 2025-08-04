baf2dc502e64e628444c270f2bdc99ba52c7a3aa
tracer-ysyx <tracer@ysyx.org>
2025-08-04 14:44:24 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:44:24 up  3:44,  1 user,  load average: 0.66, 0.41, 0.37

--- npc/vsrc/IDU/IDU.v
+++ npc/vsrc/IDU/IDU.v
@@ -315,6 +315,16 @@ module IDU (
                 csr_addr = 12'b0;
             end
         end
+        // 避免锁存器
+            else begin
+            imm = 32'b0;
+            rs1 = 5'b0;
+            rs2 = 5'b0;
+            rd  = 5'b0;
+            shamt = 6'b0;
+            offset = 32'b0;
+            csr_addr = 12'b0;
+        end
     end
 
     Itype Itype_u(
