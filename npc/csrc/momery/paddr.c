476531c10fae4a90cdd96f5acad8a6884ac1626a
tracer-ysyx <tracer@ysyx.org>
2025-08-04 20:26:43 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:43 up  9:01,  1 user,  load average: 2.40, 1.18, 0.74

--- npc/csrc/momery/paddr.c
+++ npc/csrc/momery/paddr.c
@@ -124,7 +124,7 @@ word_t paddr_read(paddr_t addr, int len) {
     char s[128];
     sprintf(s, "paddr_read(0x%08x, %d, 0x%08x)\n", addr, len);
     log_write("%s\n", s);
-    out_of_bound(addr);
+    // out_of_bound(addr);
   return 0;
 }
 
@@ -134,5 +134,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   mmio_write(addr, len, data);
   return;
   #endif
-  out_of_bound(addr);
+  // out_of_bound(addr);
 }
