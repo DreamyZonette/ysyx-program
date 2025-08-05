b9ef6c0d5f55c236ebf2f870318a40af3dc20992
tracer-ysyx <tracer@ysyx.org>
2025-08-05 12:35:10 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:35:10 up 13:31,  1 user,  load average: 1.75, 0.81, 0.44

--- npc/csrc/isa/reg.c
+++ npc/csrc/isa/reg.c
@@ -24,10 +24,10 @@ void isa_reg_display() {
 		}
 	}
 	printf("\n");
-	printf("\033[32mmstatus\033[0m: \033[33m%s\033[0m\t", top->de_mstatus);
-	printf("\033[32mmcause\033[0m: \033[33m%s\033[0m\t", top->de_mcause);
-	printf("\033[32mmepc\033[0m: \033[33m%s\033[0m\t", top->de_mepc);
-	printf("\033[32mmtvec\033[0m: \033[33m%s\033[0m\t", top->de_mtvec);
+	printf("\033[32mmstatus\033[0m: \033[33m%08x\033[0m\t", top->de_mstatus);
+	printf("\033[32mmcause\033[0m: \033[33m%08x\033[0m\t", top->de_mcause);
+	printf("\033[32mmepc\033[0m: \033[33m%08x\033[0m\t", top->de_mepc);
+	printf("\033[32mmtvec\033[0m: \033[33m%08x\033[0m\t", top->de_mtvec);
 	printf("\n");
 }
 
