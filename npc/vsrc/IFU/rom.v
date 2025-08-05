ba099551017f92807d39fdca6eee4a051dc9ab9a
tracer-ysyx <tracer@ysyx.org>
2025-08-05 19:56:07 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:56:07 up 19:35,  1 user,  load average: 0.12, 0.25, 0.46

--- npc/vsrc/IFU/rom.v
+++ npc/vsrc/IFU/rom.v
@@ -1,32 +1,32 @@
-// /* verilator lint_off DECLFILENAME */
-// module rom (
-//     input [31:0] addr,
-//     output reg [31:0] rdata
-// );
-//     wire [31:0] rom_out [255:0];
-//     wire [7:0] rom_offset;
+/* verilator lint_off DECLFILENAME */
+module rom (
+    input [31:0] addr,
+    output reg [31:0] rdata
+);
+    wire [31:0] rom_out [255:0];
+    wire [7:0] rom_offset;
 
-//     assign rom_offset = addr[7:0];
+    assign rom_offset = addr[7:0];
 
-//     genvar i;
-//     generate
-//         for (i = 0; i < 256; i = i + 1) begin : rom_mem
-//         Reg #(
-//         .WIDTH(32),        // 设置寄存器宽度为32位
-//         .RESET_VAL(0)      // 复位值为0（可自定义）
-//         ) u_reg (
-//         .clk(clk),
-//         .sys_rst_n(sys_rst_n),
-//         .i_data(i_data),   // 所有寄存器共享数据输入
-//         .o_data(rom_out[i]),// 独立输出到总线数组
-//         .wen(1'b0)   // 独立的地址译码写使能
-//         );
-//     end
-//     endgenerate
+    genvar i;
+    generate
+        for (i = 0; i < 256; i = i + 1) begin : rom_mem
+        Reg #(
+        .WIDTH(32),        // 设置寄存器宽度为32位
+        .RESET_VAL(0)      // 复位值为0（可自定义）
+        ) u_reg (
+        .clk(clk),
+        .sys_rst_n(sys_rst_n),
+        .i_data(i_data),   // 所有寄存器共享数据输入
+        .o_data(rom_out[i]),// 独立输出到总线数组
+        .wen(1'b0)   // 独立的地址译码写使能
+        );
+    end
+    endgenerate
 
-//     always @(*) begin
-//         rdata = rom_out[rom_offset];
-//     end
+    always @(*) begin
+        rdata = rom_out[rom_offset];
+    end
 
     
 
@@ -34,5 +34,5 @@
 
 
 
-// endmodule
-//  /* verilator lint_on DECLFILENAME */
+endmodule
+ /* verilator lint_on DECLFILENAME */
