91208c24aff346d4cdef5fc8603830ea507f1e6f
tracer-ysyx <tracer@ysyx.org>
2025-08-08 18:59:15 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:15 up  8:11,  1 user,  load average: 0.23, 0.35, 0.36

--- npc/vsrc/LSU/sram.v
+++ npc/vsrc/LSU/sram.v
@@ -107,14 +107,14 @@ always @(posedge i_sys_clk) begin
         end
 
         READ: begin
-            // if(count == DELAY-1) begin
-            if(count == delay_count-1) begin
+            if(count == DELAY-1) begin
+            // if(count == delay_count-1) begin
                 sram_data <= pmem_read(araddr, 4);
                 rvalid <= 1;
                 rresp <= 2'b00; // 认为每一次都会成功
             end
-            // if(count >= DELAY && i_rready) begin // 握手完成
-            if(count >= delay_count && i_rready) begin // 握手完成
+            if(count >= DELAY && i_rready) begin // 握手完成
+            // if(count >= delay_count && i_rready) begin // 握手完成
                 state <= IDLE;
                 rvalid <= 0;
                 count <= 0;
@@ -123,8 +123,8 @@ always @(posedge i_sys_clk) begin
             end
 
             WRITE: begin
-            // if(count == DELAY-1) begin
-            if(count == delay_count-1) begin
+            if(count == DELAY-1) begin
+            // if(count == delay_count-1) begin
                 /* verilator lint_off WIDTHEXPAND */
                 pmem_write(awaddr, wstrb, wdata);
                 /* verilator lint_on WIDTHEXPAND */
@@ -132,8 +132,8 @@ always @(posedge i_sys_clk) begin
                 bresp <= 2'b00; // 认为每一次都会成功
                 state <= RESP;
             end
-            // if(count >= DELAY) state <= RESP;
-            if(count >= delay_count) state <= RESP;
+            if(count >= DELAY) state <= RESP;
+            // if(count >= delay_count) state <= RESP;
             else count <= count + 1;
             end
             // if(count < DELAY) count <= count + 1;
@@ -150,23 +150,23 @@ always @(posedge i_sys_clk) begin
   end
 end
 
-reg [4:0] lsfr_data;
-reg [11:0] delay_count;
-always @(posedge i_sys_clk) begin
-    if (!i_sys_rst_n) begin
-        lsfr_data <= 5'b11111;
-        delay_count <= DELAY;
-    end
-    else if (state == IDLE)begin
-        lsfr_data <= {lsfr_data[3:0], lsfr_data[4] ^ lsfr_data[1]};
-            /* verilator lint_off WIDTHEXPAND */
-        delay_count <= lsfr_data + DELAY;
-            // $strobe("delay_count = %d", delay_count);
-            /* verilator lint_on WIDTHEXPAND */
-    end
-    else begin
-        lsfr_data <= {lsfr_data[3:0], lsfr_data[4] ^ lsfr_data[1]};
-    end
-end
+// reg [4:0] lsfr_data;
+// reg [11:0] delay_count;
+// always @(posedge i_sys_clk) begin
+//     if (!i_sys_rst_n) begin
+//         lsfr_data <= 5'b11111;
+//         delay_count <= DELAY;
+//     end
+//     else if (state == IDLE)begin
+//         lsfr_data <= {lsfr_data[3:0], lsfr_data[4] ^ lsfr_data[1]};
+//             /* verilator lint_off WIDTHEXPAND */
+//         delay_count <= lsfr_data + DELAY;
+//             // $strobe("delay_count = %d", delay_count);
+//             /* verilator lint_on WIDTHEXPAND */
+//     end
+//     else begin
+//         lsfr_data <= {lsfr_data[3:0], lsfr_data[4] ^ lsfr_data[1]};
+//     end
+// end
 
 endmodule
