c1a023a4568241f234aa48215b27f8aa913dac75
tracer-ysyx <tracer@ysyx.org>
2025-08-09 18:06:33 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:33 up  7:07,  1 user,  load average: 0.30, 0.22, 0.24

---
+++ npc/vsrc/Xbar.v
@@ -0,0 +1,10 @@
+// module Xbar (
+
+// );
+
+// localparam SRAM_LEFT    = 32'h8000_0000;
+// localparam SRAM_RIGHT   = 32'h87FF_FFFF;
+// localparam UART_LEFT    = 32'ha000_03F8;
+// localparam UART_RIGHT   = 32'ha000_0400;
+
+// endmodule
