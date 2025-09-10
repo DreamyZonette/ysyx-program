a3f22c0c36446302588bebb2f278e13b8f3514a3
tracer-ysyx <tracer@ysyx.org>
2025-09-11 00:00:45 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:00:45 up  2:44,  1 user,  load average: 1.59, 1.03, 0.69

--- npc/vsrc/IFU/ysyx_25020042_IFU.v
+++ npc/vsrc/IFU/ysyx_25020042_IFU.v
@@ -6,7 +6,7 @@ module ysyx_25020042_IFU(
     input lsu_ready,
     input wbu_ready,
     output reg ifu_valid,
-    output wire [31:0] o_instruction,
+    output reg [31:0] o_instruction,
     output reg [31:0] ifu_addr,
     // output reg ifu_wen,
     input [31:0] ifu_rdata,
@@ -21,7 +21,7 @@ localparam IDLE = 1'b0;
 localparam WAIT = 1'b1;
 
 reg state;
-assign o_instruction = ifu_rdata;
+//assign o_instruction = ifu_rdata;
 
 always @(posedge clock) begin
     if(reset) begin
@@ -30,6 +30,7 @@ always @(posedge clock) begin
         // ifu_wen <= 1'b0;
         ifu_addr <= 32'h30000000;
         ifu_reqValid <= 1'b0;
+        o_instruction <= 32'h0;
     end
     else begin
         case(state)
@@ -57,6 +58,7 @@ always @(posedge clock) begin
                 if (ifu_respValid) begin
                     state <= IDLE;
                     ifu_valid <= 1'b1;
+                    o_instruction <= ifu_rdata;
                 end
             end
             default: begin
