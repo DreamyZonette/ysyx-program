177e5f4f8f5f0c0ef0e675786b24f52e19553cc9
tracer-ysyx <tracer@ysyx.org>
2025-09-13 20:47:00 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:00 up 13:14,  1 user,  load average: 1.92, 1.18, 0.82

--- soc-npc/vsrc/IDU/ysyx_25020042_gpr.v
+++ soc-npc/vsrc/IDU/ysyx_25020042_gpr.v
@@ -17,11 +17,12 @@ module ysyx_25020042_gpr  (
 
     export "DPI-C" function get_register_value;
     function int unsigned get_register_value(input int idx);
-        if (idx >= 0 && idx <= 15) begin
-            return reg_file[idx];
-        end else begin
-            return 32'h0;
-        end
+    $display("当前模块的完整路径: %m");
+        // if (idx >= 0 && idx <= 15) begin
+        //     return reg_file[idx];
+        // end else begin
+        //     return 32'h0;
+        // end
     endfunction
 
     assign wen = (i_rd != 5'b0) && wbu_valid? (16'b1 << i_rd) : 16'b0; // 写使能信号
