47860782270d83dc1ea42335c92cec355cfdf1c2
tracer-ysyx <tracer@ysyx.org>
2025-09-13 23:18:28 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:18:28 up 15:46,  1 user,  load average: 1.13, 1.00, 0.88

--- soc-npc/vsrc/ysyx_25020042.v
+++ soc-npc/vsrc/ysyx_25020042.v
@@ -15,24 +15,13 @@
         input [31:0]  io_lsu_rdata
     );
 
-    // import "DPI-C" function void dpi_ebreak();
-    // // import "DPI-C" function void dpi_return();
+    import "DPI-C" function void dpi_ebreak();
 
     always @(posedge clock) begin
             if (ebreak_signal == 1'b1) begin
                 dpi_ebreak();
             end
     end
-
-    // assign halt = unknown_signal;
-    // assign de_pc = pc;
-    // assign de_next_pc = next_pc;
-    // assign de_inst = instruction;
-    // assign de_mstatus = mstatus;
-    // assign de_mtvec = mtvec;
-    // assign de_mepc = mepc;
-    // assign de_mcause = mcause;
-
 
     wire wbu_valid;
     wire pc_valid;
