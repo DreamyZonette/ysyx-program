2a02198d444b02186be652e3efd58935ba87b93d
tracer-ysyx <tracer@ysyx.org>
2025-09-18 09:40:30 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:40:30 up  1:31,  1 user,  load average: 0.42, 0.66, 0.74

--- soc-npc/csrc/cpu/dut.c
+++ soc-npc/csrc/cpu/dut.c
@@ -22,7 +22,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
   for(int i = 0; i < 16; i ++){
     uint32_t ref_value = ref_r->gpr[i];
-    uint32_t value = gpr(i);
+    uint32_t value = _gpr(i);
     if(value != ref_value) {
     printf("gpr[%d] \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", i, value, ref_value);
     return false;
