7846855b452c8779df48b85908bbf3ba13d37d07
tracer-ysyx <tracer@ysyx.org>
2025-09-18 09:14:28 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:14:28 up  1:05,  1 user,  load average: 1.41, 1.52, 1.23

--- soc-npc/csrc/momery/paddr.c
+++ soc-npc/csrc/momery/paddr.c
@@ -5,8 +5,8 @@
 
 static uint8_t flash_mem[CONFIG_FLASH_SIZE] PG_ALIGN = {};
 
-uint8_t* guest_to_host(paddr_t paddr) { return flash_mem + paddr; }
-paddr_t host_to_guest(uint8_t *haddr) { return haddr - flash_mem; }
+uint8_t* guest_to_host(paddr_t paddr) { return flash_mem + paddr - CONFIG_FLASH_BASE; }
+paddr_t host_to_guest(uint8_t *haddr) { return haddr - flash_mem + CONFIG_FLASH_BASE; }
 
 void init_mem() {
   memset(flash_mem, rand(), CONFIG_FLASH_SIZE);
