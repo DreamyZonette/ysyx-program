aead511ec3d274176051a5bf778d25f0baf3232f
tracer-ysyx <tracer@ysyx.org>
2025-09-21 14:53:55 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:55 up  2:02,  1 user,  load average: 0.68, 0.67, 0.70

--- soc-npc/csrc/monitor/monitor.c
+++ soc-npc/csrc/monitor/monitor.c
@@ -177,6 +177,10 @@ void init_monitor(int argc, char *argv[]) {
   init_difftest(diff_so_file, img_size, difftest_port);
   #endif
 
+  #if CONFIG_ITRACE
+  init_disasm();
+  #endif
+
   // init_device();
   
   welcome();
