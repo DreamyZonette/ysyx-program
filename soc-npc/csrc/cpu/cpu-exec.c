4ba7ebbb606bd15a045f0a5ad57013b0981ffdd5
tracer-ysyx <tracer@ysyx.org>
2025-09-21 15:37:09 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:09 up  2:46,  1 user,  load average: 1.73, 1.18, 0.81

--- soc-npc/csrc/cpu/cpu-exec.c
+++ soc-npc/csrc/cpu/cpu-exec.c
@@ -135,6 +135,13 @@ void assert_fail_msg() {
 static void execute(uint64_t n) {
     if(n <= MAX_INST_TO_PRINT) print_on = 1;
   for (;n > 0; n --) {
+    uint32_t prev_pc = dut.next_pc;
+    printf("now:0x%08x\n", prev_pc);
+    while(!sim_finish){
+      single_cycle();
+      if(prev_pc != get_pc()) break;
+    }
+
       dut.pc = dut.next_pc;
       dut.next_pc = get_pc();
     if (dut.pc != dut.next_pc) g_nr_guest_inst ++;
@@ -184,13 +191,7 @@ static void execute(uint64_t n) {
   
 
   #endif
-    uint32_t prev_pc = dut.next_pc;
-    printf("now:0x%08x\n", prev_pc);
-    while(!sim_finish){
-      single_cycle();
-      
-      if(prev_pc != get_pc()) break;
-    }
+    
     
     device_update();
     trace_and_difftest();
