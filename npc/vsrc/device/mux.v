42a9f121fe81a19359a81fe5db0150e923ae09ec
tracer-ysyx <tracer@ysyx.org>
2025-09-23 20:47:50 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:50 up  9:29,  6 users,  load average: 1.37, 1.26, 0.88

--- npc/vsrc/device/mux.v
+++ npc/vsrc/device/mux.v
@@ -1,3 +1,4 @@
+/* verilator lint_off MODDUP */
 // 选择器模板内部实现
 module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) (
   output reg [DATA_LEN-1:0] out,
@@ -53,3 +54,4 @@ module MuxKeyWithDefault #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
 );
   MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
 endmodule
+/* verilator lint_on MODDUP */
