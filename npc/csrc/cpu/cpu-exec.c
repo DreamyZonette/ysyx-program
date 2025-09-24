a71a6aa4b4cdd1552da4587660b22f855c621388
tracer-ysyx <tracer@ysyx.org>
2025-09-24 14:58:43 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:58:43 up  4:34,  1 user,  load average: 0.46, 0.29, 0.55

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -48,7 +48,7 @@ void step_and_dump_wave(){
 static void trace_and_difftest() {
 
   #if CONFIG_DIFFTEST
-  printf("0x%08x 0x%08x\n", top->de_pc, top->de_next_pc);
+  printf("difftest:0x%08x => 0x%08x\n", dut.pc, dut.next_pc);
   if (dut.pc != dut.next_pc){
     difftest_step(dut.pc, dut.next_pc);
   } 
