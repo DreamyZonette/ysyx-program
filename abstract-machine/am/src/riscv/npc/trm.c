89353a99fdfe12d6ed5d9b8f64560827e4b7f180
tracer-ysyx <tracer@ysyx.org>
2025-09-29 23:02:51 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:02:51 up  3:28,  1 user,  load average: 1.12, 0.72, 0.40

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -49,12 +49,26 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 #define UART_BASE 0x10000000
+#define THR_ADDR (UART_BASE + 0x0)
+#define LER_ADDR (UART_BASE + 0x1)
+#define IIR_ADDR (UART_BASE + 0x2)
+#define FCR_ADDR (UART_BASE + 0x2)
+#define LCR_ADDR (UART_BASE + 0x3)
+#define MCR_ADDR (UART_BASE + 0x4)
+#define LSR_ADDR (UART_BASE + 0x5)
+#define MSR_ADDR (UART_BASE + 0x6)
+#define LSB_ADDR (UART_BASE + 0x0)
+#define MSB_ADDR (UART_BASE + 0x1)
+
+// void putch(char ch) {
+//   outb(SERIAL_PORT, ch);
+// }
 
 void putch(char ch) {
-  // while ((inb(UART_BASE + 0x3) & 0x20) == 0) {
-  //       // 空循环，等待LSR[5] (THRE) 位为1
-  //   }
-  outb(SERIAL_PORT, ch);
+  while ((inb(LSR_ADDR) & 0x20) == 0) {
+        // 空循环，等待LSR[5] (THRE) 位为1
+    }
+  outb(THR_ADDR, ch);
 }
 
 void halt(int code) {
@@ -65,13 +79,15 @@ void halt(int code) {
 
 
 void _trm_init() {
-  outb(UART_BASE + 0x3, 0x83); // LCR
-  outb(UART_BASE + 0x0, 0x36); // LSB
-  outb(UART_BASE + 0x1, 0x00); // MSB
-  outb(UART_BASE + 0x3, 0x03); // LCR
-  outb(UART_BASE + 0x1, 0x00); // IER
-  outb(UART_BASE + 0x2, 0x07); // FCR
-  outb(UART_BASE + 0x4, 0x00); // MCR
+  // 配置除数寄存器
+  outb(LCR_ADDR, 0x80); // LCR
+  outb(LSB_ADDR, 0x36); // LSB
+  outb(MSB_ADDR, 0x00); // MSB
+
+  outb(LCR_ADDR, 0x03); // LCR
+  outb(FCR_ADDR, 0x07);
+  outb(MCR_ADDR, 0x03); 
+  outb(LER_ADDR, 0x00);
   int ret = main(mainargs);
   halt(ret);
 }
