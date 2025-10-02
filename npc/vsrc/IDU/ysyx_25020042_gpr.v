7290b693cb707a28e422f086d5fccc8d0afda6ca
tracer-ysyx <tracer@ysyx.org>
2025-10-02 20:07:45 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:45 up  4:28,  1 user,  load average: 1.32, 1.51, 1.17

--- npc/vsrc/IDU/ysyx_25020042_gpr.v
+++ npc/vsrc/IDU/ysyx_25020042_gpr.v
@@ -24,9 +24,9 @@ module ysyx_25020042_gpr  (
             return 32'h0;
         end
     endfunction
-    always @(posedge clock ) begin
-         $display("当前模块的完整路径: %m");
-    end
+    // always @(posedge clock ) begin
+    //      $display("当前模块的完整路径: %m");
+    // end
 
     assign wen = (i_rd != 5'b0) && wbu_valid? (16'b1 << i_rd) : 16'b0; // 写使能信号
 
