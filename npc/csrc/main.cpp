d39755972a288ed45d3dc0609783e25831d8559d
tracer-ysyx <tracer@ysyx.org>
2025-10-04 19:15:00 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:15:00 up  1:45,  1 user,  load average: 1.00, 0.95, 0.63

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -30,6 +30,7 @@ void sim_init(){
     top->trace(tfp,0);
     tfp->open("/home/long/ysyx-workbench/npc/build/wave.vcd");
     #endif
+    // svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu.IFU_u"));
 }
 
 void sim_exit(){
