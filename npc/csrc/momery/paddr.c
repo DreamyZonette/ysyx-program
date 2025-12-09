f746ab657dabb0890e3a25e5caf782fdc9cd4daa
tracer-ysyx <tracer@ysyx.org>
2025-12-09 21:43:20 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:20 up 18:01,  1 user,  load average: 0.77, 0.40, 0.36

--- npc/csrc/momery/paddr.c
+++ npc/csrc/momery/paddr.c
@@ -149,6 +149,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
   *data = internal_pmem_read(addr + 0x60000000, 4);
-  printf("mrom_read addr = 0x%08x  data = 0x%08x\n", addr, *data);
+  // printf("mrom_read addr = 0x%08x  data = 0x%08x\n", addr, *data);
   // assert(0); 
 }
