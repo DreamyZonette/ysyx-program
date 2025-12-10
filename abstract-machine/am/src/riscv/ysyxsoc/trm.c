ee6dd34d8ef1b706b9773a01bef307f2c02da7d0
tracer-ysyx <tracer@ysyx.org>
2025-12-10 22:01:07 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:01:07 up 19:57,  1 user,  load average: 1.32, 0.81, 0.66

---
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -0,0 +1,31 @@
+#include <am.h>
+#include <klib-macros.h>
+#include <riscv/riscv.h>
+
+# define DEVICE_BASE 0x10000000
+#define SERIAL_PORT     (DEVICE_BASE + 0x0000000)
+
+extern char _heap_start;
+int main(const char *args);
+
+extern char _pmem_start;
+#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+
+Area heap = RANGE(&_heap_start, PMEM_END);
+static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
+
+void putch(char ch) {
+  outb(SERIAL_PORT, ch);
+}
+
+void halt(int code) {
+  register long a0 asm("a0") = code;
+  asm volatile("ebreak" : : "r"(a0));
+  while (1);
+}
+
+void _trm_init() {
+  int ret = main(mainargs);
+  halt(ret);
+}
