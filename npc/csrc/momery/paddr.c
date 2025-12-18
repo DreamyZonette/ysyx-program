26c926876d70b5e208517836086f829f6529a734
tracer-ysyx <tracer@ysyx.org>
2025-12-19 00:19:08 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:19:08 up  1:47,  1 user,  load average: 0.71, 0.97, 0.92

--- npc/csrc/momery/paddr.c
+++ npc/csrc/momery/paddr.c
@@ -50,7 +50,7 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-#if CONFIG_YSYXSOC
+#if !CONFIG_YSYXSOC
 void init_mem() {
   memset(pmem, rand(), CONFIG_MSIZE);
   Log("physical memory area [%08x, %08x]", PMEM_LEFT, PMEM_RIGHT);
