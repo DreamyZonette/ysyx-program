e8f567b2c527aa034613418a864c1d59aa308474
tracer-ysyx <tracer@ysyx.org>
2026-01-24 19:48:39 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:48:39 up  4:48,  1 user,  load average: 0.98, 0.63, 0.50

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -73,7 +73,15 @@ void _uart_init() {
 }
 
 void _trm_init() {
-  _uart_init();
+  // _uart_init();
+  outb(LCR_ADDR, 0x80); // LCR
+  outb(LSB_ADDR, 0x36); // LSB
+  outb(MSB_ADDR, 0x00); // MSB
+
+  outb(LCR_ADDR, 0x03); // LCR
+  outb(FCR_ADDR, 0x07);
+  outb(MCR_ADDR, 0x03); 
+  outb(LER_ADDR, 0x00);
   // printf("Hello, world!\n");
   _boot_loader();
   int ret = main(mainargs);
