408c59b0aef0d93ac8083beb9c7e4bb25b6739e4
tracer-ysyx <tracer@ysyx.org>
2026-01-24 17:10:30 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:30 up  3:48,  1 user,  load average: 1.76, 0.94, 0.74

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -40,11 +40,11 @@ void halt(int code) {
 extern unsigned char _data_start[];   // .data段SRAM运行地址（VMA）起始
 extern unsigned char _data_end[];     // .data段SRAM运行地址（VMA）结束
 extern unsigned char _rodata_end[];  // .stack段SRAM运行地址（VMA）起始
-
+#define SRAM_BASE 0x0f000000
 void _boot_loader() {
-    unsigned char *src = _rodata_end;       // 源地址：ROM中的.data初始值（LMA）
-    unsigned char *dst = _data_start;  // 目标地址：SRAM中的.data运行地址（VMA）
-    while (dst < _data_end) {
+    unsigned char *src = _data_start;       // 源地址：ROM中的.data初始值（LMA）
+    unsigned char *dst = SRAM_BASE;  // 目标地址：SRAM中的.data运行地址（VMA）
+    while (src < _data_end) {
         *dst++ = *src++;  // 逐字节复制（兼容任意位宽数据）
     }
 }
