88fcdc7f248aef61d5a13e9e7bad9886e22bb7c0
tracer-ysyx <tracer@ysyx.org>
2026-01-24 21:02:22 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:22 up  6:02,  1 user,  load average: 1.00, 0.90, 0.91

--- npc/csrc/cpu/dut.c
+++ npc/csrc/cpu/dut.c
@@ -86,7 +86,7 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
   __attribute__((visibility("default")))
-  uint8_t mrom[];
+  uint8_t mrom[CONFIG_MROM_SIZE];
   int ret = fread(mrom_guest_to_host(MROM_RESET_VECTOR), 4 * 1024, 1, mrom);
   assert(ret == 1);
 
