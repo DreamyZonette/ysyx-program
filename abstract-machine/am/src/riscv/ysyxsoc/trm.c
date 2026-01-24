42f79452697a87f34aef96964056c1fc722fff4f
tracer-ysyx <tracer@ysyx.org>
2026-01-24 19:36:33 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:33 up  4:36,  1 user,  load average: 0.69, 0.45, 0.42

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -18,7 +18,7 @@ extern char _heap_start;
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_SIZE (4 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
@@ -56,6 +56,7 @@ void _boot_loader() {
     while (dst < _bss_end) {
         *dst++ = 0;  // 逐字节清零（保证所有未初始化变量为0）
     }
+    putch("Y");
 }
 
 void _uart_init() {
