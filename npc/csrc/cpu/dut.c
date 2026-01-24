d40ef6d89f7b08ae6c7e26755f12535822d40ec5
tracer-ysyx <tracer@ysyx.org>
2026-01-24 21:06:24 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:24 up  6:06,  1 user,  load average: 0.57, 0.74, 0.84

--- npc/csrc/cpu/dut.c
+++ npc/csrc/cpu/dut.c
@@ -8,6 +8,7 @@
 #include <isa/isa_def.h>
 #include <isa/reg.h>
 
+
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
@@ -83,11 +84,15 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
   }
 }
 
+extern static char img_file[256];
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
   __attribute__((visibility("default")))
   uint8_t mrom[CONFIG_MROM_SIZE];
-  int ret = fread(mrom_guest_to_host(MROM_RESET_VECTOR), 4 * 1024, 1, mrom);
+
+  FILE *fp = fopen(img_file, "rb");
+
+  int ret = fread(mrom_guest_to_host(mrom), 4 * 1024, 1, fp);
   assert(ret == 1);
 
   void *handle;
