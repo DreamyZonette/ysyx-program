7e0ca5e0b7dc37fbe5d212243641370dbd1daa7b
tracer-ysyx <tracer@ysyx.org>
2026-01-25 18:52:21 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:52:21 up  7:41,  1 user,  load average: 0.17, 0.17, 0.24

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -62,8 +62,8 @@ extern unsigned char _bss_end[];      // .bss段SRAM地址结束
 void _uart_init() {
   // 配置除数寄存器
   outb(LCR_ADDR, 0x80); // LCR
-  outb(LSB_ADDR, 0x01); // LSB
   outb(MSB_ADDR, 0x00); // MSB
+  outb(LSB_ADDR, 0x01); // LSB
 
   outb(LCR_ADDR, 0x03); // LCR
   outb(FCR_ADDR, 0x07);
