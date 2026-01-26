13c63abfee776efca0256c61e1d9c52c1ebec1ce
tracer-ysyx <tracer@ysyx.org>
2026-01-26 11:32:27 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:27 up 13:09,  1 user,  load average: 2.20, 0.98, 0.65

--- npc/vsrc/LSU/ysyx_25020042_LSU.v
+++ npc/vsrc/LSU/ysyx_25020042_LSU.v
@@ -126,9 +126,10 @@ always @(posedge clock) begin
                     lsu_ready <= 1'b1;
                         lsu_wdata <= wdata[31:0];
                         lsu_wstrb <= wstrb[3:0];
-                        lsu_araddr <= {i_data[31:2], 2'b00};
-                        lsu_awaddr <= {i_data[31:2], 2'b00};
-                    // end
+                        // lsu_araddr <= {i_data[31:2], 2'b00};
+                        // lsu_awaddr <= {i_data[31:2], 2'b00};
+                        lsu_araddr <= i_data;
+                        lsu_awaddr <= i_data;                    // end
                     if (wen) begin
                         lsu_awvalid <= 1'b1;
                         lsu_wvalid <= 1'b1;
