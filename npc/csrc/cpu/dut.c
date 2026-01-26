7547d5e0fde63f5c736571394a45262565985756
tracer-ysyx <tracer@ysyx.org>
2026-01-26 10:04:08 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:04:08 up 11:40,  1 user,  load average: 0.40, 0.51, 0.54

--- npc/csrc/cpu/dut.c
+++ npc/csrc/cpu/dut.c
@@ -29,6 +29,10 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     return false;
     }
   }
+  if(ref_r->pc != pc){
+    printf("PC \33[1;31mdut:0x%08x \33[1;32mref:0x%08x\n", i, pc, ref_r->pc);
+    return false;
+  }
 
   return true;
 }
