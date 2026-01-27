b7650f6e3eb2d1b04f47643cb7bf38d5b3b18a38
tracer-ysyx <tracer@ysyx.org>
2026-01-27 17:41:32 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:41:32 up 1 day,  2:15,  1 user,  load average: 1.49, 1.04, 0.72

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -74,9 +74,9 @@ static long load_img() {
 
     fseek(temp_fp, 0, SEEK_SET); // 回到文件开头
     long temp_size = 0;
-    fseek(fp, 0, SEEK_END);
-    temp_size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
+    fseek(temp_fp, 0, SEEK_END);
+    temp_size = ftell(temp_fp);
+    fseek(temp_fp, 0, SEEK_SET);
     int temp_ret = fread(flash_guest_to_host(FLASH_RESET_VECTOR), temp_size, 1, temp_fp);
     assert(temp_ret == 1);
   /*---------temp--------*/
