f7afc15bcfc0f2fd9ab02f7361e23ce859cdd32b
tracer-ysyx <tracer@ysyx.org>
2026-02-06 17:29:39 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:29:39 up 18:01,  1 user,  load average: 1.28, 1.19, 1.18

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -5,7 +5,7 @@
 #define KBD_ADDR        (0x10011000)
 
 #define KEYDOWN_MASK 0x8000
-#define KEYCODE_MASK 0x7FFF
+#define KEYCODE_MASK 0xFFFF
 
 static uint32_t am_get_keycode (){
   uint32_t keycode = inl(KBD_ADDR);
