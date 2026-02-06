e745ee79066bc823531d756e85773f4636d65cb8
tracer-ysyx <tracer@ysyx.org>
2026-02-06 09:43:20 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:43:20 up 11:57,  1 user,  load average: 0.27, 0.69, 0.75

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -78,16 +78,17 @@ int main(int argc, char *argv[]){
 
   
     sim_init();
+
     init_monitor(argc, argv);
 
     npc_engine_start();
     // reset(10);
 
-  while(1) {
-    nvboard_update();
-    top->clock = 0; top->eval();
-    top->clock = 1; top->eval();
-  }
+//   while(1) {
+//     nvboard_update();
+//     top->clock = 0; top->eval();
+//     top->clock = 1; top->eval();
+//   }
 //--------------------------------------
     
 
