f4f5c17f1eb4206be1a8073ef6858178aab506c2
tracer-ysyx <tracer@ysyx.org>
2026-02-06 17:26:32 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:32 up 17:58,  1 user,  load average: 1.64, 1.08, 1.15

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -5,7 +5,7 @@
 #define KBD_ADDR        (0x10011000)
 
 #define KEYDOWN_MASK 0x8000
-#define KEYCODE_MASK 0xFFFF
+#define KEYCODE_MASK 0x7FFF
 
 static uint32_t am_get_keycode (){
   uint32_t keycode = inl(KBD_ADDR);
@@ -102,14 +102,10 @@ static uint32_t key_code_lut(uint32_t keycode){
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t value = am_get_keycode();
-  if (value == 0xe0){
-    value = (value << 8) + am_get_keycode();
-  }
-  else {
-    kbd->keydown = (value & KEYDOWN_MASK) != 0;
-    uint32_t raw_code = key_code_lut(value & KEYCODE_MASK);
-    kbd->keycode = raw_code;
-  }
+  
+  kbd->keydown = (value & KEYDOWN_MASK) != 0;
+  uint32_t raw_code = key_code_lut(value & KEYCODE_MASK);
+  kbd->keycode = raw_code;
 
   if (kbd->keycode == 0) {
     kbd->keycode = AM_KEY_NONE;
