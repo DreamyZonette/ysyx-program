45f83fdd517f2aa11ba0539fb6551d97ba6e017b
tracer-ysyx <tracer@ysyx.org>
2026-02-06 23:06:05 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:06:05 up 23:37,  1 user,  load average: 1.28, 1.50, 1.44

--- npc/vsrc/axi_arbiter.v
+++ npc/vsrc/axi_arbiter.v
@@ -247,6 +247,7 @@ module axi_arbiter (
         else if (state == ARB_LSU) begin
             if (clint_active) begin
                 $display("clint_active");
+                 $display("io_lsu_arvalid_reg = %d", io_lsu_arvalid_reg);
                 io_clint_araddr =  io_lsu_araddr;
                 io_clint_arvalid = io_lsu_arvalid_reg;
                 io_clint_arid =    io_lsu_arid;
