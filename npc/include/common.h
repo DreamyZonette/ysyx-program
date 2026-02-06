77c6cffa5c66d69495081e85ccbc74ee30437448
tracer-ysyx <tracer@ysyx.org>
2026-02-06 09:46:56 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:46:56 up 12:00,  1 user,  load average: 0.78, 0.70, 0.74

--- npc/include/common.h
+++ npc/include/common.h
@@ -12,9 +12,7 @@
 #include "svdpi.h"
 #include "VysyxSoCFull__Dpi.h"
 #include "verilated_fst_c.h"
-#if CONFIG_NVBOARD
-#include <nvboard.h>
-#endif
+#include <nvboard.h> 
 
 
 typedef uint32_t word_t;
