49421965fd66e9aa8a25232b6f6996318a455fe5
tracer-ysyx <tracer@ysyx.org>
2026-02-07 17:00:17 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:17 up  3:23,  1 user,  load average: 1.91, 1.47, 1.39

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -17,8 +17,8 @@ static void am_get_gpu_config() {
     // uint32_t config = inl(VGACTL_ADDR);
     // width = 512;
     // height = 640;
-    width = 240;
-    height = 320;
+    width = 480;
+    height = 640;
 }
 
 void __am_gpu_init() {
