74326974687244ac450c736765682f86d1065d61
tracer-ysyx <tracer@ysyx.org>
2026-02-09 12:36:01 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:36:01 up 20:12,  1 user,  load average: 1.24, 1.11, 1.09

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -7,7 +7,7 @@
 #define CONFIG_DIFFTEST 0
 #define CONFIG_DEVICE 0
 #define CONFIG_LOG 1
-#define PLATFORM_YSYXSOC
+// #define PLATFORM_YSYXSOC
 // #define CONFIG_NVBOARD 1
 
 #define CONFIG_HAS_SERIAL 0
