72e456bef1ebe9bb90e17310f0bc9beb8ee31a8f
tracer-ysyx <tracer@ysyx.org>
2026-02-10 10:47:17 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:17 up 21:54,  1 user,  load average: 0.90, 0.63, 0.70

--- npc/csrc/momery/paddr.c
+++ npc/csrc/momery/paddr.c
@@ -125,14 +125,32 @@ extern "C" void pmem_write(int addr, int len, int data) {
   addr = paddr_t(addr);
   word_t wdata = word_t(data);
   // data = word_t(data);
+  int shift_len = len;
+  while (shift_len % 2 == 0) {
+    wdata = wdata >> 8;
+    addr = addr + 1;
+    shift_len = shift_len >> 1;
+  }
   switch (len) {
     case 0b1111:
       len = 4;
       break;
-    case 0b11:
+    case 0b0011:
       len = 2;
       break;
-    case 0b1:
+    case 0b1100:
+      len = 2;
+      break;
+    case 0b0001:
+      len = 1;
+      break;
+    case 0b0010:
+      len = 1;
+      break;
+    case 0b0100:
+      len = 1;
+      break;
+    case 0b1000:
       len = 1;
       break;
   }
