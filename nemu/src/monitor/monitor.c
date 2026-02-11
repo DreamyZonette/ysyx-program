ac0533119ee199b5b897f69af95f585a7258a14e
tracer-ysyx <tracer@ysyx.org>
2026-02-11 21:15:36 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:15:36 up 1 day, 17:47,  1 user,  load average: 0.70, 0.83, 0.63

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -157,9 +157,10 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Perform ISA dependent initialization. */
   init_isa();
-
+  printf("init_isa done\n");
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
+  printf("init_monitor done\n");
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
