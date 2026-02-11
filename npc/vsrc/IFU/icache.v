7276a891aa1b97058a94cb25429c2fe646e1610e
tracer-ysyx <tracer@ysyx.org>
2026-02-11 14:42:54 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:42:54 up 1 day, 11:14,  1 user,  load average: 0.57, 0.50, 0.50

--- npc/vsrc/IFU/icache.v
+++ npc/vsrc/IFU/icache.v
@@ -156,7 +156,7 @@ always @(posedge clock) begin
         io_icache_arid <= 4'h0;
         io_icache_arsize <= 3'b010;
         io_icache_arburst <= 2'b00; // INCR 01
-        io_icache_arlen <= 8'h1; // 4beat  3
+        io_icache_arlen <= 8'h0; // 4beat  3
     end
     else begin
         if (state == IDLE && !hit && pc_valid) begin
@@ -164,7 +164,7 @@ always @(posedge clock) begin
                 io_icache_araddr  <= pc_addr;
                 io_icache_arvalid <= 1'b1;
                 io_icache_arburst <= 2'b01; // INCR 01
-                io_icache_arlen   <= 8'h0; // 4beat  3
+                io_icache_arlen   <= 8'h1; // 4beat  3
             end
             else begin
                 io_icache_arburst <= 2'b00; // INCR 01
