98a98c5e2b877f8f0a7f7bbc74ea87195577a59b
tracer-ysyx <tracer@ysyx.org>
2026-02-17 17:38:18 +0800

    > compile NEMU ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:18 up  8:27,  1 user,  load average: 1.24, 0.92, 0.84

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -206,6 +206,7 @@ word_t paddr_read(paddr_t addr, int len) {
     if (addr >= FLASH_BASE && addr < FLASH_BASE + FLASH_SIZE) return flash_read(addr, len);
     if (addr >= PSRAM_BASE && addr < PSRAM_BASE + PSRAM_SIZE) return psram_read(addr, len);
     if (addr >= SDRAM_BASE && addr < SDRAM_BASE + SDRAM_SIZE) return sdram_read(addr, len);
+    printf("paddr_read addr:%x\n", addr);
     if (addr == 0x10000005) return 1;
  
     // if (addr == 0x10000005) return 0x20;// uart lsr return 0x20 
