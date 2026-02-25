bd2db77fe92eb5e6fd145ed990d6eb3afe0ede95
tracer-ysyx <tracer@ysyx.org>
2026-02-25 14:16:44 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:16:44 up 1 day, 16:26,  1 user,  load average: 0.98, 1.19, 1.15

--- npc/include/isa/reg.h
+++ npc/include/isa/reg.h
@@ -14,6 +14,25 @@ static inline int check_reg_idx(int idx) {
 
 static inline uint32_t get_register_value (int idx) {
   #ifdef PLATFORM_YSYXSOC
+  switch (idx) {
+    case 0: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__zero;
+    case 1: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__ra;
+    case 2: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__sp;
+    case 3: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__gp;
+    case 4: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__tp;
+    case 5: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__t0;
+    case 6: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__t1; 
+    case 7: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__t2;
+    case 8: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__s0;
+    case 9: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__s1;
+    case 10: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__a0;
+    case 11: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__a1;
+    case 12: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__a2;
+    case 13: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__a3;
+    case 14: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__a4;
+    case 15: return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__gpr_u__DOT__a5;
+    default: return 0;
+  }
 
   #else
   switch (idx) {
@@ -39,6 +58,10 @@ static inline uint32_t get_register_value (int idx) {
 }
 
 #ifdef PLATFORM_YSYXSOC
+#define _mstatus_data_ top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__csr_u__DOT__mstatus
+#define _mtvec_data_ top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__csr_u__DOT__mtvec
+#define _mepc_data_ top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__csr_u__DOT__mepc
+#define _mcause_data_ top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__csr_u__DOT__mcause
 
 #else
 #define _mstatus_data_ top->rootp->ysyx_25020042__DOT__csr_u__DOT__mstatus
