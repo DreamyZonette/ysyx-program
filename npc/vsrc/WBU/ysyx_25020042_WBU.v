70173ed79eafb97240ebdc264c3da7953223e9be
tracer-ysyx <tracer@ysyx.org>
2026-02-25 16:23:21 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:23:21 up 1 day, 18:33,  1 user,  load average: 0.74, 0.67, 1.13

--- npc/vsrc/WBU/ysyx_25020042_WBU.v
+++ npc/vsrc/WBU/ysyx_25020042_WBU.v
@@ -8,22 +8,16 @@ module ysyx_25020042_WBU(
     input [31:0]      i_data,
     input [31:0]      i_pc_data,
     input  [7:0]      i_inst,
-    // input  [4:0]      i_rd,
-    // input  [31:0]     i_csr_rdata,
     `ifdef VERILATOR
     input  [31:0]     i_instruction_data,
     `endif
     input  [11:0]     i_csr_addr,
-    // output reg [4:0]  o_rd,
     output reg [31:0] csr_wdata,
     output reg [11:0] csr_addr,
-    // output reg [31:0] reg_wdata,
     output reg [31:0] o_mepc_wdata,
     output reg [31:0] o_mcause_wdata
 );
 
-    // localparam  JUMP_INST    = 3'b010;
-    // localparam  MEM_INST     = 3'b011;
     localparam  CSR_INST     = 3'b100;
 
     localparam IDLE = 1'b0;
@@ -53,11 +47,9 @@ module ysyx_25020042_WBU(
 always @(posedge clock) begin
     if(reset) begin
         state <= IDLE;
-        // reg_wdata <= 32'b0;
         csr_wdata <= 32'b0;
         wbu_ready <= 1'b1;
         wbu_valid <= 1'b0;
-        // o_rd <= 5'b0;
         o_mepc_wdata <= 32'b0;
         o_mcause_wdata <= 32'b0;
         csr_addr <= 12'b0;
@@ -69,14 +61,7 @@ always @(posedge clock) begin
                     state <= WAIT;
                     wbu_ready <= 1'b0;
                     wbu_valid <= 1'b1;
-                    // o_rd <= i_rd;
                     case(i_inst[7:5])
-                        // JUMP_INST: begin
-                        //     if (i_inst[4:0] == 5'b00001 || i_inst[4:0] == 5'b00010) begin
-                        //         reg_wdata <= i_pc_data + 4;
-                        //     end
-                        // end
-
                         CSR_INST: begin
                             case (i_inst[4:0])
                                 5'b00011: begin // ecall
@@ -86,17 +71,11 @@ always @(posedge clock) begin
                                 default: begin
                                     csr_wdata <= i_data;
                                     csr_addr <= i_csr_addr;
-                                    // reg_wdata <= i_csr_rdata;
                                 end
                             endcase
                         end
 
-                        // MEM_INST: begin
-                        //     reg_wdata <= i_data;
-                        // end
-
                         default: begin
-                            // reg_wdata <= i_data;
                         end
                     endcase
 
