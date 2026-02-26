dccd69d4857c69b9c7e97142539bb8631f6bb7b4
tracer-ysyx <tracer@ysyx.org>
2026-02-26 12:28:06 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:28:06 up 2 days,  3:12,  1 user,  load average: 0.56, 0.94, 1.02

--- npc/vsrc/ipc_counter.v
+++ npc/vsrc/ipc_counter.v
@@ -22,7 +22,7 @@ module ipc_counter(
     parameter icache_access_time = 1;
     parameter icache_miss_penalty = 30; // 36 apb delay_on dram // 19 axiburst
 
-    wire [63:0] EXU_instructions = ifu_performance_counter - csr_performance_counter - lsu_performance_counter;
+    wire [63:0] EXU_instructions = exu_performance_counter - csr_performance_counter - lsu_performance_counter;
     wire [63:0] CSR_instructions = csr_performance_counter;
     wire [63:0] LSU_instructions = lsu_performance_counter;
 
