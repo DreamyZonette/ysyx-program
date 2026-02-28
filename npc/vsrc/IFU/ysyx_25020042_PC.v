e1747308a9e2eaa8e934d8f002aedb75d104b392
tracer-ysyx <tracer@ysyx.org>
2026-02-28 23:00:06 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:00:06 up 41 min,  1 user,  load average: 1.56, 1.04, 0.91

--- npc/vsrc/IFU/ysyx_25020042_PC.v
+++ npc/vsrc/IFU/ysyx_25020042_PC.v
@@ -40,10 +40,13 @@ module ysyx_25020042_PC #(PC_LEN = 32)(
             next_pc <= 32'h8000_0004;
             `endif
         end
-        else if (i_jump_valid & icache_busy) begin
-            next_pc <= i_jump_pc;
-        end
-        else if (i_jump_valid & !icache_busy) begin
+        // else if (i_jump_valid & icache_busy) begin
+        //     next_pc <= i_jump_pc;
+        // end
+        // else if (i_jump_valid & !icache_busy) begin
+        //     next_pc <= i_jump_pc + 4;
+        // end
+        else if (i_jump_valid) begin
             next_pc <= i_jump_pc + 4;
         end
         else if (ifu_ready & pc_valid) begin
@@ -64,11 +67,14 @@ module ysyx_25020042_PC #(PC_LEN = 32)(
             if (fault)begin
                 o_pc <= 0;
             end
-            else if (i_jump_valid & !icache_busy)begin
+            // else if (i_jump_valid & !icache_busy)begin
+            //     o_pc <= i_jump_pc;
+            // end
+            // else if (jump_signal & !icache_busy) begin
+            //     o_pc <= next_pc;
+            // end
+            else if (i_jump_valid) begin
                 o_pc <= i_jump_pc;
-            end
-            else if (jump_signal & !icache_busy) begin
-                o_pc <= next_pc;
             end
             else if (ifu_handsake)begin
                 o_pc <= next_pc;
@@ -76,9 +82,11 @@ module ysyx_25020042_PC #(PC_LEN = 32)(
 
             if (ifu_handsake) 
                 pc_valid <= 1'b1;
-            else if (i_jump_valid & !icache_busy)
-                pc_valid <= 1'b1;
-            else if (jump_signal & !icache_busy)
+            // else if (i_jump_valid & !icache_busy)
+            //     pc_valid <= 1'b1;
+            // else if (jump_signal & !icache_busy)
+            //     pc_valid <= 1'b1;
+            else if (jump_signal)
                 pc_valid <= 1'b1;
             else if (ifu_ready & pc_valid) 
                 pc_valid <= 1'b0;
