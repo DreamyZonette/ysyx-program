34e1e8b7c382900364395c3a1d92f6f9766fdd06
tracer-ysyx <tracer@ysyx.org>
2026-03-08 12:05:13 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:05:13 up 21:32,  1 user,  load average: 1.38, 1.55, 1.12

--- npc/csrc/cpu/cpu-exec.c
+++ npc/csrc/cpu/cpu-exec.c
@@ -57,9 +57,9 @@ static void trace_and_difftest() {
   int is_call = 0;
   int is_ret = 0;
 
-  uint32_t opcode = get_instruction() & 0x7F;
-  uint32_t rd = get_instruction() >> 7 & 0x1F;
-  uint32_t rs1 = (get_instruction()) >> 15 & 0x1F;
+  uint32_t opcode = _instruction_data_ & 0x7F;
+  uint32_t rd = _instruction_data_ >> 7 & 0x1F;
+  uint32_t rs1 = (_instruction_data_) >> 15 & 0x1F;
  
 
   if ((opcode == 0x6F && rd == 1) ||  // JAL rd=x1
