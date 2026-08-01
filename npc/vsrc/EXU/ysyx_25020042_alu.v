c29d73298561d4276c34b18153aab4fb47b24735
tracer-ysyx <tracer@ysyx.org>
2026-08-01 22:06:46 +0800

    > sim RTL ysyx_25020042 龙文庆 Linux LONG 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:06:46 up 1 day,  1:16,  1 user,  load average: 2.29, 1.66, 1.24

--- npc/vsrc/EXU/ysyx_25020042_alu.v
+++ npc/vsrc/EXU/ysyx_25020042_alu.v
@@ -57,7 +57,6 @@ module comparer (
     input [31:0] y,
     output        less_out
 );
-// 翻转符号位把有符号比较变成无符号比较，避免 $signed，面积更小
 wire [31:0] x_cmp = {x[31] ^ sign, x[30:0]};
 wire [31:0] y_cmp = {y[31] ^ sign, y[30:0]};
 assign less_out = (x_cmp < y_cmp) ? 1'b1 : 1'b0;
@@ -78,35 +77,45 @@ endmodule
 module barrel_shifter_param (
     input  logic_en,
     input  Right,
-    input  [31:0] data_i,   
-    input  [4:0]  shift_amt, 
-    output [31:0] data_o     
+    input  [31:0] data_i,
+    input  [4:0]  shift_amt,
+    output [31:0] data_o
 );
 
 wire sign = logic_en ? 1'b0 : data_i[31];
 
+wire [31:0] data_rev = {
+    data_i[ 0], data_i[ 1], data_i[ 2], data_i[ 3],
+    data_i[ 4], data_i[ 5], data_i[ 6], data_i[ 7],
+    data_i[ 8], data_i[ 9], data_i[10], data_i[11],
+    data_i[12], data_i[13], data_i[14], data_i[15],
+    data_i[16], data_i[17], data_i[18], data_i[19],
+    data_i[20], data_i[21], data_i[22], data_i[23],
+    data_i[24], data_i[25], data_i[26], data_i[27],
+    data_i[28], data_i[29], data_i[30], data_i[31]
+};
+
+wire        eff_sign   = Right ? sign   : 1'b0;
+wire [31:0] shift_in   = Right ? data_i : data_rev;
+
 wire [31:0] stage0, stage1, stage2, stage3, stage4;
-wire [31:0] Lstage0, Lstage1, Lstage2, Lstage3, Lstage4;
-// 右
-assign stage0 = shift_amt[0] ? {sign, data_i[31:1]} : data_i;
+assign stage0 = shift_amt[0] ? {{1{eff_sign}}, shift_in[31:1]}   : shift_in;
+assign stage1 = shift_amt[1] ? {{2{eff_sign}}, stage0[31:2]}     : stage0;
+assign stage2 = shift_amt[2] ? {{4{eff_sign}}, stage1[31:4]}     : stage1;
+assign stage3 = shift_amt[3] ? {{8{eff_sign}}, stage2[31:8]}     : stage2;
+assign stage4 = shift_amt[4] ? {{16{eff_sign}}, stage3[31:16]}   : stage3;
 
-assign stage1 = shift_amt[1] ? {{2{sign}}, stage0[31:2]} : stage0;
+wire [31:0] stage4_rev = {
+    stage4[ 0], stage4[ 1], stage4[ 2], stage4[ 3],
+    stage4[ 4], stage4[ 5], stage4[ 6], stage4[ 7],
+    stage4[ 8], stage4[ 9], stage4[10], stage4[11],
+    stage4[12], stage4[13], stage4[14], stage4[15],
+    stage4[16], stage4[17], stage4[18], stage4[19],
+    stage4[20], stage4[21], stage4[22], stage4[23],
+    stage4[24], stage4[25], stage4[26], stage4[27],
+    stage4[28], stage4[29], stage4[30], stage4[31]
+};
 
-assign stage2 = shift_amt[2] ? {{4{sign}}, stage1[31:4]} : stage1;
+assign data_o = Right ? stage4 : stage4_rev;
 
-assign stage3 = shift_amt[3] ? {{8{sign}}, stage2[31:8]} : stage2;
-
-assign stage4 = shift_amt[4] ? {{16{sign}}, stage3[31:16]} : stage3;
-// 左
-assign Lstage0 = shift_amt[0] ? {data_i[30:0], 1'b0} : data_i;
-
-assign Lstage1 = shift_amt[1] ? {Lstage0[29:0], 2'b0} : Lstage0;
-
-assign Lstage2 = shift_amt[2] ? {Lstage1[27:0], 4'b0}: Lstage1;
-
-assign Lstage3 = shift_amt[3] ? {Lstage2[23:0], 8'b0}: Lstage2;
-
-assign Lstage4 = shift_amt[4] ? {Lstage3[15:0], 16'b0} : Lstage3;
-
-assign data_o = Right ? stage4 : Lstage4; 
 endmodule
